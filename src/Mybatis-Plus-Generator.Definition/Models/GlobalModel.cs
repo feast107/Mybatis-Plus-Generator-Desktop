3b735cfded0eb0c9bd06b038bd672be10c52a73f
Feast <feast107@gmail.com>
2023-04-25 13:30:47 +0800

    Create GlobalModel.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Models/GlobalModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Mybatis_Plus_Generator.Definition.Models
+{
+    public partial class GlobalModel : ObservableObject
+    {
+        [ObservableProperty] private string? outputDir;
+        [ObservableProperty] private string? author;
+        [ObservableProperty] private bool enableKotlin;
+        [ObservableProperty] private bool enableSwagger = true;
+        [ObservableProperty] private bool enableSpringDoc = true;
+        [ObservableProperty] private int dateType;
+        [ObservableProperty] private string commentDate = "yyyy-MM-dd";
+    }
+}
