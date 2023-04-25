51268852ea64b91ac403a0b71f0e994792d3ee02
Feast <feast107@gmail.com>
2023-04-25 22:32:58 +0800

    Create LangConverter.cs

---
+++ src/Mybatis-Plus-Generator/Converters/LangConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Mybatis_Plus_Generator.Converters
+{
+    public class LangConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value is string key)
+                {
+                    return Langs.Lang.ResourceManager.GetString(key) ?? key;
+                }
+            }
+            catch { 
+                //
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
