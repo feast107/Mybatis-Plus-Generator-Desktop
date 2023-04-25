8527592f86612831163b0e9c9134719c9edeacfd
Feast <feast107@gmail.com>
2023-04-25 13:30:44 +0800

    Create DataSourceModel.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Models/DataSourceModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Mybatis_Plus_Generator.Definition.Models
+{
+    public partial class DataSourceModel : ObservableObject
+    {
+        [ObservableProperty] private string? dbUrl;
+        [ObservableProperty] private string? dbUser;
+        [ObservableProperty] private string? dbPassword;
+        [ObservableProperty] private string? schema;
+        [ObservableProperty] private string? dbQuery;
+        [ObservableProperty] private string? typeConvert;
+        [ObservableProperty] private string? keyWordsHandler;
+        [ObservableProperty] private ObservableCollection<KeyValuePair<string, string>> connectionProperties = new();
+    }
+}
