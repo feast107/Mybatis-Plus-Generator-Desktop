eea8117d0d008750d2f3c751d3c748d85afbe16c
Feast <feast107@gmail.com>
2023-04-25 13:30:55 +0800

    Update App.xaml.cs

--- src/Mybatis-Plus-Generator/App.xaml.cs
+++ src/Mybatis-Plus-Generator/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Mybatis_Plus_Generator.Core;
 
 namespace Mybatis_Plus_Generator
 {
@@ -13,5 +14,9 @@ namespace Mybatis_Plus_Generator
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            new Generator().Generate();
+        }
     }
 }
