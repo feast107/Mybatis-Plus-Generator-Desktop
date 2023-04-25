01316ff225e66a472a1cff30083e51adab31d946
Feast <feast107@gmail.com>
2023-04-25 22:32:51 +0800

    Create IConfigureService.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Interfaces/IConfigureService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mybatis_Plus_Generator.Definition.Interfaces
+{
+    public interface IConfigureService
+    {
+
+    }
+}
