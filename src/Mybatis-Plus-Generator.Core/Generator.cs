51f44951070d0d1dfbc8c1a167d4407670d014a4
Feast <feast107@gmail.com>
2023-04-25 13:30:35 +0800

    Create Generator.cs

---
+++ src/Mybatis-Plus-Generator.Core/Generator.cs
@@ -0,0 +1,12 @@
+﻿using com.baomidou.mybatisplus.generator;
+using com.baomidou.mybatisplus.generator.config;
+
+namespace Mybatis_Plus_Generator.Core
+{
+    public class Generator
+    {
+        public void Generate()
+        {
+        }
+    }
+}
