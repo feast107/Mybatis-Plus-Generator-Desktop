f97a4b1d68f074917d933ee22389fb9abb9b155b
Feast <feast107@gmail.com>
2023-04-25 22:33:40 +0800

    Create DataSourcePage.xaml.cs

---
+++ src/Mybatis-Plus-Generator/Visuals/Pages/DataSourcePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Mybatis_Plus_Generator.Visuals.Pages
+{
+    /// <summary>
+    /// DataSourcePage.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataSourcePage : Page
+    {
+        public DataSourcePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
