1f94478c73444ebbc43ee10ce9a68473e25170bd
Feast <feast107@gmail.com>
2023-04-25 16:10:44 +0800

    Create MyBatisConfigExtension.cs

---
+++ src/Mybatis-Plus-Generator.Core/Extensions/MyBatisConfigExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using com.baomidou.mybatisplus.generator.config;
+
+namespace Mybatis_Plus_Generator.Core.Extensions
+{
+    public static class MyBatisConfigExtension
+    {
+        private static readonly List<string> BuilderBaseMethods;
+
+        static MyBatisConfigExtension()
+        {
+            BuilderBaseMethods = typeof(java.lang.Object).GetMethods().Select(x => x.Name).ToList();
+            BuilderBaseMethods.AddRange(typeof(IConfigBuilder).GetMethods().Select(x => x.Name).ToList());
+        }
+
+        public static List<MethodInfo> ExportFunc<T>() where T : IConfigBuilder
+        {
+            return typeof(T).GetMethods().Where(x=>x.ReturnType == typeof(T)).ToList();
+        }
+    }
+}
