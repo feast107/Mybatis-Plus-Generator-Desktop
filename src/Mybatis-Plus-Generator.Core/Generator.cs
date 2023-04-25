5a75d6a979059422ac0d7af08d585ddaae127bd9
Feast <feast107@gmail.com>
2023-04-25 16:10:46 +0800

    Update Generator.cs

--- src/Mybatis-Plus-Generator.Core/Generator.cs
+++ src/Mybatis-Plus-Generator.Core/Generator.cs
@@ -1,5 +1,6 @@
 ﻿using com.baomidou.mybatisplus.generator;
 using com.baomidou.mybatisplus.generator.config;
+using Mybatis_Plus_Generator.Core.Extensions;
 
 namespace Mybatis_Plus_Generator.Core
 {
@@ -7,6 +8,14 @@ namespace Mybatis_Plus_Generator.Core
     {
         public void Generate()
         {
+            var dataSourceConfigs = MyBatisConfigExtension.ExportFunc<DataSourceConfig.Builder>();
+            var globalConfigs = MyBatisConfigExtension.ExportFunc<GlobalConfig.Builder>();
+            var packageConfigs = MyBatisConfigExtension.ExportFunc<PackageConfig.Builder>();
+            var injectConfigs = MyBatisConfigExtension.ExportFunc<InjectionConfig.Builder>();
+            var strategyConfigs = MyBatisConfigExtension.ExportFunc<StrategyConfig.Builder>();
+            var controllerConfigs = strategyConfigs;
+            var serviceConfigs = strategyConfigs;
+            var mapperConfigs = strategyConfigs;
         }
     }
 }
