1dce709acc226a9e756ce7f86534567f9b49125c
Feast <feast107@gmail.com>
2023-04-25 22:32:49 +0800

    Create Configurable.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/Configurable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Mybatis_Plus_Generator.Definition.Abstractions
+{
+    public abstract partial class Configurable : ObservableObject
+    {
+        [ObservableProperty] private string? field;
+        [ObservableProperty] private Type? valueType;
+        [ObservableProperty] private string? value;
+        public abstract bool AllowMultiple { get; protected set; }
+    }
+}
