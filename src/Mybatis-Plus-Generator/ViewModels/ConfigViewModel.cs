e06ee0b119207b2e1422ec9e61bbe50b8ab5839d
Feast <feast107@gmail.com>
2023-04-25 22:33:36 +0800

    Create ConfigViewModel.cs

---
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+
+namespace Mybatis_Plus_Generator.ViewModels
+{
+    public partial class ConfigViewModel : ObservableObject
+    {
+        [ObservableProperty] private string? configName;
+        [ObservableProperty] private Dictionary<string,dynamic> configs = new();
+    }
+}
