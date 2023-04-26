67283b000b43129c38f050df7878f78c119f1072
Feast <feast107@gmail.com>
2023-04-26 13:14:38 +0800

    Create ConfigInfo.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Mybatis_Plus_Generator.Definition.Abstractions
+{
+    public abstract partial class ConfigInfo : ObservableObject
+    {
+        [ObservableProperty] private TemplateInfo? templateInfo;
+
+    }
+}
