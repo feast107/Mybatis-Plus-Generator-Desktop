f4a179a01648675f42f0adbd63370151158a1db1
Feast <feast107@gmail.com>
2023-04-26 19:36:08 +0800

    Create ConfigRecord.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigRecord.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Mybatis_Plus_Generator.Definition.Abstractions
+{
+    public partial class ConfigRecord : ObservableObject
+    {
+        [ObservableProperty] private string? configName;
+        [ObservableProperty] private ConfigInfo? fixedConfig;
+        [ObservableProperty] private ObservableCollection<ConfigInfo> configs = new();
+    }
+}
