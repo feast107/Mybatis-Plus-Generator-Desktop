07efb4dd535b1acbb10e70974d2960e7bf2d66ff
Feast <feast107@gmail.com>
2023-04-26 13:15:15 +0800

    Create ITemplateService.cs

---
+++ src/Mybatis-Plus-Generator.Core/Interfaces/ITemplateService.cs
@@ -0,0 +1,13 @@
+﻿using Mybatis_Plus_Generator.Definition.Abstractions;
+using System.Collections.ObjectModel;
+
+namespace Mybatis_Plus_Generator.Core.Interfaces
+{
+    /// <summary>
+    /// 模板服务
+    /// </summary>
+    public interface ITemplateService
+    {
+        ObservableCollection<TemplateInfo> ConfigTemplates { get; }
+    }
+}
