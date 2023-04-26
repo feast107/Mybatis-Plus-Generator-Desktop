fd797b6f1524f7f59cc58b2adb4ce600cd580552
Feast <feast107@gmail.com>
2023-04-26 13:15:11 +0800

    Create ServiceExtension.cs

---
+++ src/Mybatis-Plus-Generator.Core/Extensions/ServiceExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Mybatis_Plus_Generator.Core.Interfaces;
+using Mybatis_Plus_Generator.Core.Services;
+
+namespace Mybatis_Plus_Generator.Core.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddDefaultService(this IServiceCollection collection) 
+            => collection.AddSingleton<IConfigureService, ConfigureService>()
+                .AddSingleton<ITemplateService,TemplateService>();
+    }
+}
