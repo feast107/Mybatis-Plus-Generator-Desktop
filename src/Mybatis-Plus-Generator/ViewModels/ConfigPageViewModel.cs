dde6dae753745164e78167389558b3ea9d286a5f
Feast <feast107@gmail.com>
2023-04-26 13:14:52 +0800

    Create ConfigPageViewModel.cs

---
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using CommunityToolkit.Mvvm.Input;
+using Mybatis_Plus_Generator.Definition.Abstractions;
+
+namespace Mybatis_Plus_Generator.ViewModels
+{
+    public partial class ConfigPageViewModel : ObservableObject
+    {
+        [ObservableProperty] private string? configName;
+        [ObservableProperty] private ObservableCollection<ConfigTabViewModel> configs = new();
+        [ObservableProperty] private ObservableCollection<TemplateInfo> templates = new();
+
+        [RelayCommand]
+        void SelectMenu(TemplateInfo template)
+        {
+            Debugger.Break();
+        }
+    }
+}
