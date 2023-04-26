32195916b121b9d5d659c45f85894a033cc4a18f
Feast <feast107@gmail.com>
2023-04-26 13:15:19 +0800

    Create TemplateInfo.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/TemplateInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Mybatis_Plus_Generator.Definition.Abstractions
+{
+    /// <summary>
+    /// 配置模板
+    /// </summary>
+    public partial class TemplateInfo : ObservableObject
+    {
+        /// <summary>
+        /// 模板名称
+        /// </summary>
+        [ObservableProperty] private string? name;
+        /// <summary>
+        /// 模板类型
+        /// </summary>
+        [ObservableProperty] private Type? configType;
+        /// <summary>
+        /// 配置函数
+        /// </summary>
+        [ObservableProperty] private ObservableCollection<TemplateItemInfo> fields = new();
+    }
+}
