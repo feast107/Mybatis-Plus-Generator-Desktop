77cfbf015917815b0cce1c4db150910d0b24267d
Feast <feast107@gmail.com>
2023-04-26 19:36:23 +0800

    Create ConfigRecordViewModel.cs

---
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigRecordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Mybatis_Plus_Generator.Definition.Abstractions;
+
+namespace Mybatis_Plus_Generator.ViewModels
+{
+    public partial class ConfigRecordViewModel : ConfigRecord
+    {
+    }
+}
