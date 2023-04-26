269feb33b9bda8e311de2f6dce003676cfbe779c
Feast <feast107@gmail.com>
2023-04-26 13:15:17 +0800

    Create ConfigTabViewModel.cs

---
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigTabViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Mybatis_Plus_Generator.Definition.Abstractions;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Mybatis_Plus_Generator.ViewModels
+{
+    public partial class ConfigTabViewModel : ObservableObject
+    {
+        /// <summary>
+        /// 配置类型
+        /// </summary>
+        [ObservableProperty] private string? configType;
+
+        [ObservableProperty] private ObservableCollection<ConfigInfo> configs = new();
+    }
+}
