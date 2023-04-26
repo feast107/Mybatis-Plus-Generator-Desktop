725f28bc7333d9f1338181a7fbd2f5438437086a
Feast <feast107@gmail.com>
2023-04-26 19:36:14 +0800

    Update TemplateItemInfo.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/TemplateItemInfo.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/TemplateItemInfo.cs
@@ -6,8 +6,17 @@ namespace Mybatis_Plus_Generator.Definition.Abstractions
 {
     public partial class TemplateItemInfo : ObservableObject
     {
+        /// <summary>
+        /// 配置项名称
+        /// </summary>
         [ObservableProperty] private string? fieldName;
+        /// <summary>
+        /// 是否允许多个
+        /// </summary>
         [ObservableProperty] bool allowMultiple;
+        /// <summary>
+        /// 调用方法
+        /// </summary>
         [ObservableProperty] private ObservableCollection<MethodInfo> methods = new();
     }
 }
