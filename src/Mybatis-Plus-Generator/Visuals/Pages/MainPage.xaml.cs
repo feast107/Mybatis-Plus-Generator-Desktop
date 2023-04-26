27d1aa594a1d929d2702a3619301fbdd026a3779
Feast <feast107@gmail.com>
2023-04-26 13:15:04 +0800

    Create MainPage.xaml.cs

---
+++ src/Mybatis-Plus-Generator/Visuals/Pages/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Mybatis_Plus_Generator.Visuals.Pages
+{
+    /// <summary>
+    /// ConfigView.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
