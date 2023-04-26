2037e76f738938cdf0bf54a3c887a54ee908ffe2
Feast <feast107@gmail.com>
2023-04-26 19:36:21 +0800

    Update ConfigPageViewModel.cs

--- src/Mybatis-Plus-Generator/ViewModels/ConfigPageViewModel.cs
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigPageViewModel.cs
@@ -1,21 +1,53 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using CommunityToolkit.Mvvm.Input;
+using MaterialDesignThemes.Wpf;
 using Mybatis_Plus_Generator.Definition.Abstractions;
+using Mybatis_Plus_Generator.Visuals.Controls;
+using javax.swing.text;
+using Mybatis_Plus_Generator.Core.Interfaces;
 
 namespace Mybatis_Plus_Generator.ViewModels
 {
     public partial class ConfigPageViewModel : ObservableObject
     {
+        private IConfigureService configureService;
+
+        public ConfigPageViewModel(IConfigureService configureService){
+            this.configureService = configureService;
+        }
+
         [ObservableProperty] private string? configName;
-        [ObservableProperty] private ObservableCollection<ConfigTabViewModel> configs = new();
+        [ObservableProperty] private ObservableCollection<ConfigRecord> records = new();
+        [ObservableProperty] private ConfigRecord? current;
         [ObservableProperty] private ObservableCollection<TemplateInfo> templates = new();
 
         [RelayCommand]
-        void SelectMenu(TemplateInfo template)
+        async Task SelectTemplate(TemplateInfo template)
         {
-            Debugger.Break();
+            var dialog = new ConfigNameDialog();
+            var res = await DialogHost.Show(dialog, "NewConfig");
+            if (res is not true) return;
+            Current!.Configs.Add(configureService.Instantiate(template,dialog.Title.Text));
         }
+
+        [RelayCommand]
+        async Task RemoveConfig(ConfigInfo config)
+        {
+            if (config.Equals(Current!.FixedConfig))
+            {
+                 await DialogHost.Show(new SimpleDialog("Can not delete fixed config"), "NewConfig");
+                 return;
+            }
+
+            Current.Configs.Remove(config);
+        }
+
+        public Func<Task> ShowDialog { get; init; }
     }
 }
