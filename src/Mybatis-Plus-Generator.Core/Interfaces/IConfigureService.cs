2e1ef3d55803c9e447417c576dfa70c1d95bdf4c
Feast <feast107@gmail.com>
2023-04-26 13:15:12 +0800

    Create IConfigureService.cs

---
+++ src/Mybatis-Plus-Generator.Core/Interfaces/IConfigureService.cs
@@ -0,0 +1,15 @@
+﻿using com.baomidou.mybatisplus.generator.config.builder;
+using Mybatis_Plus_Generator.Definition.Abstractions;
+using System.Collections.ObjectModel;
+
+namespace Mybatis_Plus_Generator.Core.Interfaces
+{
+    public interface IConfigureService
+    {
+        ConfigInfo DataSourceConfig { get; }
+
+        ObservableCollection<ConfigInfo> GetConfigures<T>(string field) where T : ConfigBuilder;
+
+        List<Type> GetConfiguresTypes();
+    }
+}
