f655c097e27cd7b9d9ac03177cad83871d71364c
Feast <feast107@gmail.com>
2023-04-26 13:14:59 +0800

    Create ConfigView.xaml.cs

---
+++ src/Mybatis-Plus-Generator/Visuals/Controls/ConfigView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Mybatis_Plus_Generator.Visuals.Controls
+{
+    /// <summary>
+    /// ConfigView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ConfigView : UserControl
+    {
+        public ConfigView()
+        {
+            InitializeComponent();
+        }
+    }
+}
