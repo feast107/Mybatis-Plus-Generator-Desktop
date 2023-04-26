5c7ab8c4318337a414842862e242f78f69cf8f59
Feast <feast107@gmail.com>
2023-04-26 19:36:09 +0800

    Update ConfigInfo.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigInfo.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigInfo.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Mybatis_Plus_Generator.Definition.Abstractions
 {
-    public abstract partial class ConfigInfo : ObservableObject
+    public partial class ConfigInfo : ObservableObject
     {
+        /// <summary>
+        /// 关联模板
+        /// </summary>
         [ObservableProperty] private TemplateInfo? templateInfo;
-
+        /// <summary>
+        /// 配置项
+        /// </summary>
+        [ObservableProperty] private ObservableCollection<ConfigItemInfo> configItems = new();
+        /// <summary>
+        /// 配置名称
+        /// </summary>
+        [ObservableProperty] private string? configName;
     }
 }
