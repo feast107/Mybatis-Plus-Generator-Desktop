6f7f311284c36d90884f718037cd27ef8214b618
Feast <feast107@gmail.com>
2023-04-26 19:36:03 +0800

    Create ConfigItemInfo.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Mybatis_Plus_Generator.Definition.Abstractions
+{
+    public partial class ConfigItemInfo : ObservableObject
+    {
+        /// <summary>
+        /// 关联模板
+        /// </summary>
+        [ObservableProperty] private TemplateItemInfo? templateInfo;
+        /// <summary>
+        /// 值
+        /// </summary>
+        [ObservableProperty] private string? value;
+        /// <summary>
+        /// 值类型
+        /// </summary>
+        [ObservableProperty] private Type? valueType;
+    }
+}
