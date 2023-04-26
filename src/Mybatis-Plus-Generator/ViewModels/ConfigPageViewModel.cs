31613fc1dfac7bca12678c8f42b98adb2c07405b
Feast <feast107@gmail.com>
2023-04-27 00:08:21 +0800

    Update ConfigPageViewModel.cs

--- src/Mybatis-Plus-Generator/ViewModels/ConfigPageViewModel.cs
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigPageViewModel.cs
@@ -2,21 +2,24 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using com.sun.org.apache.bcel.@internal.generic;
 using CommunityToolkit.Mvvm.Input;
 using MaterialDesignThemes.Wpf;
 using Mybatis_Plus_Generator.Definition.Abstractions;
 using Mybatis_Plus_Generator.Visuals.Controls;
 using javax.swing.text;
 using Mybatis_Plus_Generator.Core.Interfaces;
+using Mybatis_Plus_Generator.Langs;
 
 namespace Mybatis_Plus_Generator.ViewModels
 {
     public partial class ConfigPageViewModel : ObservableObject
     {
-        private IConfigureService configureService;
+        private readonly IConfigureService configureService;
 
         public ConfigPageViewModel(IConfigureService configureService){
             this.configureService = configureService;
@@ -33,6 +36,18 @@ namespace Mybatis_Plus_Generator.ViewModels
             var dialog = new ConfigNameDialog();
             var res = await DialogHost.Show(dialog, "NewConfig");
             if (res is not true) return;
+            string name = dialog.Title.Text;
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                await SimpleDialog.Show(Lang.Please_provide_config_name, "NewConfig");
+                return;
+            }
+
+            if (Current!.Configs.FirstOrDefault(x => x.ConfigName == name) != null)
+            {
+                await SimpleDialog.Show(Lang.Config_name_exists, "NewConfig");
+                return;
+            }
             Current!.Configs.Add(configureService.Instantiate(template,dialog.Title.Text));
         }
 
@@ -41,13 +56,13 @@ namespace Mybatis_Plus_Generator.ViewModels
         {
             if (config.Equals(Current!.FixedConfig))
             {
-                 await DialogHost.Show(new SimpleDialog("Can not delete fixed config"), "NewConfig");
+                 await DialogHost.Show(
+                     new SimpleDialog(Lang.Can_not_delete_fixed_config),
+                     "NewConfig");
                  return;
             }
 
             Current.Configs.Remove(config);
         }
-
-        public Func<Task> ShowDialog { get; init; }
     }
 }
