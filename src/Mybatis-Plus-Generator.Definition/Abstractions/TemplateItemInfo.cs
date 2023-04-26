3fbdfb72b2c1e3a9e61a856a6832484aa4d254ac
Feast <feast107@gmail.com>
2023-04-26 13:14:44 +0800

    Create TemplateItemInfo.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/TemplateItemInfo.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+namespace Mybatis_Plus_Generator.Definition.Abstractions
+{
+    public partial class TemplateItemInfo : ObservableObject
+    {
+        [ObservableProperty] private string? fieldName;
+        [ObservableProperty] bool allowMultiple;
+        [ObservableProperty] private ObservableCollection<MethodInfo> methods = new();
+    }
+}
