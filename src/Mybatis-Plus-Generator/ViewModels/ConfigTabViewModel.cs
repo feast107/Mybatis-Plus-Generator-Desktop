3b8d9e36918aaf6bc6db9f03809d278ff60a6047
Feast <feast107@gmail.com>
2023-04-27 19:46:23 +0800

    Update ConfigTabViewModel.cs

--- src/Mybatis-Plus-Generator/ViewModels/ConfigTabViewModel.cs
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigTabViewModel.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Mybatis_Plus_Generator.Definition.Abstractions;
 using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
-using CommunityToolkit.Mvvm.Input;
 
 namespace Mybatis_Plus_Generator.ViewModels
 {
@@ -15,5 +12,7 @@ namespace Mybatis_Plus_Generator.ViewModels
         [ObservableProperty] private string? configType;
 
         [ObservableProperty] private ObservableCollection<ConfigInfo> configs = new();
+
+
     }
 }
