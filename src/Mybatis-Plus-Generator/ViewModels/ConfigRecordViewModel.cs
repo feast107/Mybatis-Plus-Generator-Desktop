0581d31d29b99684f64647312ce785010ae9e699
Feast <feast107@gmail.com>
2023-04-27 23:26:55 +0800

    Update ConfigRecordViewModel.cs

--- src/Mybatis-Plus-Generator/ViewModels/ConfigRecordViewModel.cs
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigRecordViewModel.cs
@@ -1,62 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using MaterialDesignThemes.Wpf;
-using Mybatis_Plus_Generator.Core.Interfaces;
-using Mybatis_Plus_Generator.Definition.Abstractions;
-using Mybatis_Plus_Generator.Langs;
-using Mybatis_Plus_Generator.Visuals.Controls;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Mybatis_Plus_Generator.Definition.Abstractions;
 
 namespace Mybatis_Plus_Generator.ViewModels
 {
-    public partial class ConfigRecordViewModel : ObservableObject
+    internal class ConfigRecordViewModel 
+        : ConfigRecord<ConfigInfoViewModel, ConfigItemInfoViewModel>
     {
-        private readonly IConfigureService<ConfigInfoViewModel> configureService;
-
-        public ConfigRecordViewModel(IConfigureService<ConfigInfoViewModel> configureService){
-            this.configureService = configureService;
-        }
-
-        [ObservableProperty] private string? configName;
-        [ObservableProperty] private ObservableCollection<ConfigRecord> records = new();
-        [ObservableProperty] private ConfigRecord? current;
-        [ObservableProperty] private ObservableCollection<TemplateInfo> templates = new();
-
-        [RelayCommand]
-        async Task SelectTemplate(TemplateInfo template)
-        {
-            var dialog = new ConfigNameDialog();
-            var res = await DialogHost.Show(dialog, "NewConfig");
-            if (res is not true) return;
-            var name = dialog.Title.Text;
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                await SimpleDialog.Show(Lang.Please_provide_config_name, "NewConfig");
-                return;
-            }
-
-            if (Current!.Configs.FirstOrDefault(x => x.ConfigName == name) != null)
-            {
-                await SimpleDialog.Show(Lang.Config_name_exists, "NewConfig");
-                return;
-            }
-            Current!.Configs.Add(configureService.Instantiate(template,dialog.Title.Text));
-        }
-
-        [RelayCommand]
-        async Task RemoveConfig(ConfigInfo config)
-        {
-            if (config.Equals(Current!.FixedConfig))
-            {
-                 await DialogHost.Show(
-                     new SimpleDialog(Lang.Can_not_delete_fixed_config),
-                     "NewConfig");
-                 return;
-            }
-
-            Current.Configs.Remove(config);
-        }
     }
 }
