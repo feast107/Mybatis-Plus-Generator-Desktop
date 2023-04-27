99eabcf80b643177dfa08b030670848f34c1d2c3
Feast <feast107@gmail.com>
2023-04-27 19:46:22 +0800

    Update IConfigureService.cs

--- src/Mybatis-Plus-Generator.Core/Interfaces/IConfigureService.cs
+++ src/Mybatis-Plus-Generator.Core/Interfaces/IConfigureService.cs
@@ -3,10 +3,10 @@ using Mybatis_Plus_Generator.Definition.Abstractions;
 
 namespace Mybatis_Plus_Generator.Core.Interfaces
 {
-    public interface IConfigureService
+    public interface IConfigureService<out TConfigInfo> where TConfigInfo : ConfigInfo
     {
         ObservableCollection<ConfigRecord> Records { get; }
         ConfigRecord CreateRecord(string name);
-        ConfigInfo Instantiate(TemplateInfo template, string name);
+        TConfigInfo Instantiate(TemplateInfo template, string name);
     }
 }
