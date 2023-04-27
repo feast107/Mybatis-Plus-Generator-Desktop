95725210887a00d0f890e1b521f33a420fee6b72
Feast <feast107@gmail.com>
2023-04-27 23:26:48 +0800

    Create ConfigItemInfoViewModel.cs

---
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigItemInfoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Mybatis_Plus_Generator.Definition.Abstractions;
+using System.Linq;
+
+namespace Mybatis_Plus_Generator.ViewModels
+{
+    internal class ConfigItemInfoViewModel : ConfigItemInfo
+    {
+        /// <summary>
+        /// 可选
+        /// </summary>
+        public bool CanSwitch => CanIgnore || !TemplateInfo!.IsCtor;
+        /// <summary>
+        /// 可以忽略
+        /// </summary>
+        public bool CanIgnore =>
+            canIgnore ??= TemplateInfo!.IsCtor && TemplateInfo.Methods.All(x => x.GetParameters().Length == 0);
+        private bool? canIgnore;
+        public SwitchCondition Condition => TemplateInfo!.AllowMultiple
+            ? IsGenerated
+                ? SwitchCondition.ShowRemove
+                : SwitchCondition.ShowAdd
+            : SwitchCondition.None;
+    }
+}
