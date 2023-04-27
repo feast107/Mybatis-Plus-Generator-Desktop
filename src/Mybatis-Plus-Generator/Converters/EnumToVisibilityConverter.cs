7e69e788447fc8f7ae39b57d053b1730ed2f99d2
Feast <feast107@gmail.com>
2023-04-27 19:46:06 +0800

    Create EnumToVisibilityConverter.cs

---
+++ src/Mybatis-Plus-Generator/Converters/EnumToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Mybatis_Plus_Generator.Converters
+{
+    public abstract class EnumToVisibilityConverter<TEnum> : IValueConverter where TEnum : Enum
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is not TEnum condition 
+                ? Visibility.Collapsed 
+                : Condition(condition);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        protected abstract Visibility Condition(TEnum condition);
+    }
+}
