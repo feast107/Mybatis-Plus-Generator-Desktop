b4a818693fc54075a4d1d739d9f47d4a6f3441ed
Feast <feast107@gmail.com>
2023-04-27 19:46:12 +0800

    Create IndexConverter.cs

---
+++ src/Mybatis-Plus-Generator/Converters/IndexConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Mybatis_Plus_Generator.Converters
+{
+    public class IndexConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)value -1);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
