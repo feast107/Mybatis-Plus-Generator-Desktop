ad65cf59dc57aabeb870bb64e3a3946ab8152800
Feast <feast107@gmail.com>
2023-04-27 23:26:42 +0800

    Update ConfigureService.cs

--- src/Mybatis-Plus-Generator.Core/Services/ConfigureService.cs
+++ src/Mybatis-Plus-Generator.Core/Services/ConfigureService.cs
@@ -6,8 +6,11 @@ using Mybatis_Plus_Generator.Definition.Abstractions;
 
 namespace Mybatis_Plus_Generator.Core.Services
 {
-    internal class ConfigureService<TConfigInfo> : 
-        IConfigureService<TConfigInfo> where TConfigInfo : ConfigInfo
+    internal class ConfigureService<TConfigRecord,TConfigInfo, TConfigItemInfo> 
+        : IConfigureService<TConfigRecord,TConfigInfo, TConfigItemInfo>
+        where TConfigRecord : ConfigRecord<TConfigInfo, TConfigItemInfo>
+        where TConfigItemInfo : ConfigItemInfo
+        where TConfigInfo : ConfigInfo<TConfigItemInfo>
     {
         private readonly ITemplateService templateService;
         public ConfigureService(ITemplateService templateService)
@@ -15,9 +18,9 @@ namespace Mybatis_Plus_Generator.Core.Services
             this.templateService = templateService;
         }
 
-        public ObservableCollection<ConfigRecord> Records { get; } = new ();
+        public ObservableCollection<TConfigRecord> Records { get; } = new ();
 
-        public ConfigRecord CreateRecord(string name)
+        public TConfigRecord CreateRecord(string name)
         {
             var record = Records.FirstOrDefault(x => x.ConfigName == name);
             if(record != null)
@@ -26,13 +29,10 @@ namespace Mybatis_Plus_Generator.Core.Services
             }
             var pt = templateService.PrimaryTemplate;
             var tmp = Instantiate(pt, pt.Name ?? "");
-            record = new ConfigRecord()
-            {
-                ConfigName = name,
-                FixedConfig = tmp,
-                Configs = { tmp }
-            };
-            
+            record = Core.Provider.GetRequiredService<TConfigRecord>();
+            record.ConfigName = name;
+            record.FixedConfig = tmp;
+            record.Configs = new ObservableCollection<TConfigInfo>() { tmp };
             Records.Add(record);
             return record;
         }
@@ -42,12 +42,12 @@ namespace Mybatis_Plus_Generator.Core.Services
             var config = Core.Provider.GetRequiredService<TConfigInfo>();
             config.TemplateInfo = template;
             config.ConfigName = name;
-            config.ConfigItems = template.Fields.Aggregate(new ObservableCollection<ConfigItemInfo>(), (o, e) =>
+            config.ConfigItems = template.Fields.Aggregate(new ObservableCollection<TConfigItemInfo>(), (o, e) =>
             {
-                o.Add(new ConfigItemInfo()
-                {
-                    TemplateInfo = e,
-                });
+                var info = Core.Provider.GetRequiredService<TConfigItemInfo>();
+                info.TemplateInfo = e;
+                info.SelectMethod = e.Methods[0];
+                o.Add(info);
                 return o;
             });
             return config;
