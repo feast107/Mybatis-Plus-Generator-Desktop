b6df2eb65b8ea6614671e01199d3b127d0877281
Feast <feast107@gmail.com>
2023-04-27 23:27:06 +0800

    Update MainWindow.xaml.cs

--- src/Mybatis-Plus-Generator/Visuals/Windows/MainWindow.xaml.cs
+++ src/Mybatis-Plus-Generator/Visuals/Windows/MainWindow.xaml.cs
@@ -12,7 +12,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Mybatis_Plus_Generator.Core.Interfaces;
 using Mybatis_Plus_Generator.Visuals.Controls;
 using MaterialDesignColors;
+using Mybatis_Plus_Generator.Definition.Abstractions;
 using Mybatis_Plus_Generator.Extension;
+using IConfigureService =
+    Mybatis_Plus_Generator.Core.Interfaces.IConfigureService<
+        Mybatis_Plus_Generator.ViewModels.ConfigRecordViewModel,
+        Mybatis_Plus_Generator.ViewModels.ConfigInfoViewModel,
+        Mybatis_Plus_Generator.ViewModels.ConfigItemInfoViewModel>;
 
 namespace Mybatis_Plus_Generator.Visuals.Windows
 {
@@ -25,21 +31,21 @@ namespace Mybatis_Plus_Generator.Visuals.Windows
         {
             InitializeComponent();
             Core.Core.Services.AddDefaultService();
-            Core.Core.Services.AddTransient<ConfigRecordViewModel>();
+            Core.Core.Services.AddTransient<ConfigItemInfoViewModel>();
             Core.Core.Services.AddTransient<ConfigInfoViewModel>();
+            Core.Core.Services.AddTransient<ConfigRecordViewModel>();
+            Core.Core.Services.AddTransient<ConfigPageViewModel>();
             Core.Core.Build();
-            var dc = Core.Core.Provider.GetRequiredService<ConfigRecordViewModel>();
-            ConfigExporter.ExportConfigs();
             var templateService = Core.Core.Provider.GetRequiredService<ITemplateService>();
-            var configureService = Core.Core.Provider.GetRequiredService<IConfigureService<ConfigInfoViewModel>>();
+            var configureService = Core.Core.Provider.GetRequiredService<IConfigureService>();
+            var dc = Core.Core.Provider.GetRequiredService<ConfigPageViewModel>();
+            this.DataContext = dc;
             dc.Records = configureService.Records;
             if (configureService.Records.Count == 0)
             {
                 dc.Current = configureService.CreateRecord("默认配置");
             }
             dc.Templates = templateService.AdditionalTemplates;
-            Page.DataContext = dc;
-
             Color.FromRgb(12, 174, 135).SetTheme();
         }
     }
