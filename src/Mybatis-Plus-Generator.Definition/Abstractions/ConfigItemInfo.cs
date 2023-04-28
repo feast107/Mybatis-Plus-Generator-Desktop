78e9a4a5647b6dc21a76ad03746e01cf39feb598
Feast <feast107@gmail.com>
2023-04-28 12:38:48 +0800

    Update ConfigItemInfo.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemInfo.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemInfo.cs
@@ -3,50 +3,49 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
 
-namespace Mybatis_Plus_Generator.Definition.Abstractions
+namespace Mybatis_Plus_Generator.Definition.Abstractions;
+
+public partial class ConfigItemInfo : ObservableObject
 {
-    public partial class ConfigItemInfo : ObservableObject
+    public enum SwitchCondition
     {
-        public enum SwitchCondition
-        {
-            ShowAdd,
-            ShowRemove,
-            None
-        }
-
-        /// <summary>
-        /// 关联模板
-        /// </summary>
-        [ObservableProperty] private TemplateItemInfo? templateInfo;
-
-        [ObservableProperty] private bool isEnable = true;
-
-        [ObservableProperty] private bool isGenerated = false;
-
-        private MethodBase ChangeSelectMethod(MethodBase method)
-        {
-            SetProperty(ref selectMethod, method);
-            var param = method.GetParameters();
-            Args = param.Aggregate(new ObservableCollection<ConfigItemArgInfo>(), (o, c) =>
-            {
-                o.Add(new ConfigItemArgInfo()
-                {
-                    ArgName = c.Name,
-                    ArgType = c.ParameterType,
-                });
-                return o;
-            });
-            return method;
-        }
-
-        public MethodBase SelectMethod
-        {
-            get => selectMethod ??= ChangeSelectMethod(TemplateInfo!.Methods[0]);
-            set => ChangeSelectMethod(value);
-        }
-
-        private MethodBase? selectMethod;
-        
-        [ObservableProperty] private ObservableCollection<ConfigItemArgInfo>? args;
+        ShowAdd,
+        ShowRemove,
+        None
     }
+
+    /// <summary>
+    /// 关联模板
+    /// </summary>
+    [ObservableProperty] private TemplateItemInfo templateInfo = null!;
+
+    [ObservableProperty] private bool isEnable = true;
+
+    [ObservableProperty] private bool isGenerated = false;
+
+    private MethodBase ChangeSelectMethod(MethodBase method)
+    {
+        SetProperty(ref selectMethod, method);
+        var param = method.GetParameters();
+        Args = param.Aggregate(new ObservableCollection<ConfigItemArgInfo>(), (o, c) =>
+        {
+            o.Add(new ConfigItemArgInfo()
+            {
+                ArgName = c.Name,
+                ArgType = c.ParameterType,
+            });
+            return o;
+        });
+        OnPropertyChanged(nameof(HasParam));
+        return method;
+    }
+    public bool HasParam => SelectMethod.GetParameters().Length != 0;
+    public MethodBase SelectMethod
+    {
+        get => selectMethod ??= ChangeSelectMethod(TemplateInfo!.Methods[0]);
+        set => ChangeSelectMethod(value);
+    }
+    private MethodBase? selectMethod;
+        
+    [ObservableProperty] private ObservableCollection<ConfigItemArgInfo> args = null!;
 }
