a5983b43008398144508d26d22e0c419404d65ca
Feast <feast107@gmail.com>
2023-04-28 15:31:27 +0800

    Create ObjectExtension.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Extensions/ObjectExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Mybatis_Plus_Generator.Definition.Extensions
+{
+    public static class ObjectExtension
+    {
+        public static object[]? AsParameter(this object? param)
+        {
+            return param == null ? null : new [] { param };
+        }
+    }
+}
