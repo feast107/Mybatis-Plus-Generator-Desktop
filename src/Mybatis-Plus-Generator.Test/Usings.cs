2fe45720f503e56d6126bdba203277020d543fba
Feast <feast107@gmail.com>
2023-04-28 15:55:34 +0800

    Update Usings.cs

--- src/Mybatis-Plus-Generator.Test/Usings.cs
+++ src/Mybatis-Plus-Generator.Test/Usings.cs
@@ -1,1 +1,11 @@
 global using NUnit.Framework;
+global using IConfigureService =
+    Mybatis_Plus_Generator.Core.Interfaces.IConfigureService<
+        Mybatis_Plus_Generator.ViewModels.ConfigRecordViewModel,
+        Mybatis_Plus_Generator.ViewModels.ConfigInfoViewModel,
+        Mybatis_Plus_Generator.ViewModels.ConfigItemInfoViewModel>;
+
+global using IGenerateService = Mybatis_Plus_Generator.Core.Interfaces.IGenerateService<
+    Mybatis_Plus_Generator.ViewModels.ConfigRecordViewModel,
+    Mybatis_Plus_Generator.ViewModels.ConfigInfoViewModel,
+    Mybatis_Plus_Generator.ViewModels.ConfigItemInfoViewModel>;
