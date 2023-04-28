f86124e80f66553ab7c67a11571db1fed264ac93
Feast <feast107@gmail.com>
2023-04-28 20:05:00 +0800

    Create ServiceExtension.cs

---
+++ src/Mybatis-Plus-Generator/Extension/ServiceExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Mybatis_Plus_Generator.ViewModels;
+
+namespace Mybatis_Plus_Generator.Extension
+{
+    internal static class ServiceExtension
+    {
+        public static IServiceCollection AddViewModels(this IServiceCollection collection)
+        {
+            return collection.AddTransient<ConfigItemInfoViewModel>()
+            .AddTransient<ConfigInfoViewModel>()
+            .AddTransient<ConfigRecordViewModel>()
+            .AddTransient<ConfigPageViewModel>();
+        }
+    }
+}
