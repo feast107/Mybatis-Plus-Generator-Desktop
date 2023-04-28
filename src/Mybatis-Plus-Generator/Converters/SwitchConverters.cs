c040234bf03d1070d7499fd1760d21a0264ae124
Feast <feast107@gmail.com>
2023-04-28 12:39:08 +0800

    Update SwitchConverters.cs

--- src/Mybatis-Plus-Generator/Converters/SwitchConverters.cs
+++ src/Mybatis-Plus-Generator/Converters/SwitchConverters.cs
@@ -1,40 +1,39 @@
 ﻿using Mybatis_Plus_Generator.Definition.Abstractions;
 using System.Windows;
 
-namespace Mybatis_Plus_Generator.Converters
-{
-    internal class SwitchAddConverter : EnumToVisibilityConverter<ConfigItemInfo.SwitchCondition>
-    {
-        protected override Visibility Condition(ConfigItemInfo.SwitchCondition condition)
-        {
-            return condition switch
-            {
-                ConfigItemInfo.SwitchCondition.ShowAdd => Visibility.Visible,
-                _ => Visibility.Collapsed
-            };
-        }
-    }
+namespace Mybatis_Plus_Generator.Converters;
 
-    internal class SwitchRemoveConverter : EnumToVisibilityConverter<ConfigItemInfo.SwitchCondition>
+internal class SwitchAddConverter : EnumToVisibilityConverter<ConfigItemInfo.SwitchCondition>
+{
+    protected override Visibility Condition(ConfigItemInfo.SwitchCondition condition)
     {
-        protected override Visibility Condition(ConfigItemInfo.SwitchCondition condition)
+        return condition switch
         {
-            return condition switch
-            {
-                ConfigItemInfo.SwitchCondition.ShowRemove => Visibility.Visible,
-                _ => Visibility.Collapsed
-            };
-        }
+            ConfigItemInfo.SwitchCondition.ShowAdd => Visibility.Visible,
+            _ => Visibility.Collapsed
+        };
     }
-    internal class SwitchNoneConverter : EnumToVisibilityConverter<ConfigItemInfo.SwitchCondition>
+}
+
+internal class SwitchRemoveConverter : EnumToVisibilityConverter<ConfigItemInfo.SwitchCondition>
+{
+    protected override Visibility Condition(ConfigItemInfo.SwitchCondition condition)
     {
-        protected override Visibility Condition(ConfigItemInfo.SwitchCondition condition)
+        return condition switch
         {
-            return condition switch
-            {
-                ConfigItemInfo.SwitchCondition.None => Visibility.Visible,
-                _ => Visibility.Collapsed
-            };
-        }
+            ConfigItemInfo.SwitchCondition.ShowRemove => Visibility.Visible,
+            _ => Visibility.Collapsed
+        };
+    }
+}
+internal class SwitchNoneConverter : EnumToVisibilityConverter<ConfigItemInfo.SwitchCondition>
+{
+    protected override Visibility Condition(ConfigItemInfo.SwitchCondition condition)
+    {
+        return condition switch
+        {
+            ConfigItemInfo.SwitchCondition.None => Visibility.Visible,
+            _ => Visibility.Collapsed
+        };
     }
 }
