2a27867bf4ab753aa72faefc101e370851ee4d2d
Feast <feast107@gmail.com>
2023-04-28 12:38:52 +0800

    Update ConfigRecord.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigRecord.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigRecord.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace Mybatis_Plus_Generator.Definition.Abstractions
+namespace Mybatis_Plus_Generator.Definition.Abstractions;
+
+public partial class ConfigRecord<TConfigInfo, TConfigItemInfo> : ObservableObject
+    where TConfigInfo : ConfigInfo<TConfigItemInfo>
+    where TConfigItemInfo : ConfigItemInfo
 {
-    public partial class ConfigRecord<TConfigInfo,TConfigItemInfo> : ObservableObject 
-        where TConfigInfo : ConfigInfo<TConfigItemInfo>
-        where TConfigItemInfo : ConfigItemInfo
-    {
-        [ObservableProperty] private string? configName;
-        [ObservableProperty] private TConfigInfo? fixedConfig;
-        [ObservableProperty] private ObservableCollection<TConfigInfo> configs = new();
-    }
+    [ObservableProperty] private string configName = string.Empty;
+    [ObservableProperty] private TConfigInfo fixedConfig = null!;
+    [ObservableProperty] private ObservableCollection<TConfigInfo> configs = new();
 }
