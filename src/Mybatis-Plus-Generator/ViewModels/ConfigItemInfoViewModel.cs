866a80e8864dcbfbd1a5a3aad5524a688a54e01d
Feast <feast107@gmail.com>
2023-04-28 12:39:11 +0800

    Update ConfigItemInfoViewModel.cs

--- src/Mybatis-Plus-Generator/ViewModels/ConfigItemInfoViewModel.cs
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigItemInfoViewModel.cs
@@ -1,43 +1,42 @@
 ﻿using Mybatis_Plus_Generator.Definition.Abstractions;
 using System.Linq;
 
-namespace Mybatis_Plus_Generator.ViewModels
+namespace Mybatis_Plus_Generator.ViewModels;
+
+internal partial class ConfigItemInfoViewModel : ConfigItemInfo
 {
-    internal partial class ConfigItemInfoViewModel : ConfigItemInfo
+    public enum InputTypes
     {
-        public enum InputTypes
-        {
-            NoArg,
-            StringArg,
-            MultiStringArg
-        }
-
-        public InputTypes InputType => SelectMethod.GetParameters().Length switch
-        {
-            0 => InputTypes.NoArg,
-            1 => InputTypes.StringArg,
-            _ => InputTypes.MultiStringArg
-        };
-
-        /// <summary>
-        /// 可选
-        /// </summary>
-        public bool ShowSwitch => 
-            !TemplateInfo!.IsCtor || CanIgnore;
-        /// <summary>
-        /// 可以忽略
-        /// </summary>
-        public bool CanIgnore => canIgnore ??= 
-            TemplateInfo!.IsCtor && TemplateInfo.Methods.Any(x => x.GetParameters().Length == 0);
-
-        private bool? canIgnore;
-
-        public SwitchCondition Condition => TemplateInfo!.AllowMultiple
-            ? IsGenerated
-                ? SwitchCondition.ShowRemove
-                : SwitchCondition.ShowAdd
-            : SwitchCondition.None;
-
-        public ConfigItemArgInfo Arg => SelectMethod.GetParameters().Length == 0 ? null : Args[0];
+        NoArg,
+        StringArg,
+        MultiStringArg
     }
+
+    public InputTypes InputType => SelectMethod.GetParameters().Length switch
+    {
+        0 => InputTypes.NoArg,
+        1 => InputTypes.StringArg,
+        _ => InputTypes.MultiStringArg
+    };
+
+    /// <summary>
+    /// 可选
+    /// </summary>
+    public bool ShowSwitch => 
+        !TemplateInfo!.IsCtor || CanIgnore;
+    /// <summary>
+    /// 可以忽略
+    /// </summary>
+    public bool CanIgnore => canIgnore ??= 
+        TemplateInfo!.IsCtor && TemplateInfo.Methods.Any(x => x.GetParameters().Length == 0);
+
+    private bool? canIgnore;
+
+    public SwitchCondition Condition => TemplateInfo!.AllowMultiple
+        ? IsGenerated
+            ? SwitchCondition.ShowRemove
+            : SwitchCondition.ShowAdd
+        : SwitchCondition.None;
+
+    public ConfigItemArgInfo Arg => SelectMethod.GetParameters().Length == 0 ? null : Args[0];
 }
