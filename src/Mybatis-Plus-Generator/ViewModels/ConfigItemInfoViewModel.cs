3225a6ee92b8aafdf4de7d4334d83aed02adebbf
Feast <feast107@gmail.com>
2023-04-29 01:24:38 +0800

    Update ConfigItemInfoViewModel.cs

--- src/Mybatis-Plus-Generator/ViewModels/ConfigItemInfoViewModel.cs
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigItemInfoViewModel.cs
@@ -1,10 +1,24 @@
 ﻿using Mybatis_Plus_Generator.Definition.Abstractions;
 using System.Linq;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Mybatis_Plus_Generator.ViewModels;
 
 internal partial class ConfigItemInfoViewModel : ConfigItemInfo
 {
+    public ConfigItemInfoViewModel()
+    {
+        base.PropertyChanged += (o,e) =>
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(SelectMethod):
+                    OnPropertyChanged(nameof(Arg));
+                    OnPropertyChanged(nameof(ArgType));
+                    break;
+            }
+        };
+    }
     public enum ArgTypes
     {
         NoArg,
@@ -22,15 +36,26 @@ internal partial class ConfigItemInfoViewModel : ConfigItemInfo
     /// <summary>
     /// 可选
     /// </summary>
-    public bool ShowSwitch => 
-        !TemplateItemInfo!.IsCtor || CanIgnore;
+    public bool ShowSwitch => !TemplateItemInfo!.IsCtor || CanIgnore;
     /// <summary>
     /// 可以忽略
     /// </summary>
     public bool CanIgnore => canIgnore ??= 
         TemplateItemInfo!.IsCtor && TemplateItemInfo.Methods.Any(x => x.GetParameters().Length == 0);
-
     private bool? canIgnore;
+
+    public bool CanChangeSelect => TemplateItemInfo.Methods.Count > 1;
+
+    [RelayCommand]
+    private void NextMethod()   
+    {
+        var index = TemplateItemInfo.Methods.IndexOf(SelectMethod) + 1;
+        if (index >= TemplateItemInfo.Methods.Count)
+        {
+            index = 0;
+        }
+        SelectMethod = TemplateItemInfo.Methods[index];
+    }
 
     public SwitchCondition Condition => TemplateItemInfo!.AllowMultiple
         ? IsGenerated
