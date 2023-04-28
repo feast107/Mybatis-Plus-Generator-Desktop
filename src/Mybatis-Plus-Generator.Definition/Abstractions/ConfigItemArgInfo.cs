91485782fcd1c5e15f85b29db79b5712851bc2e2
Feast <feast107@gmail.com>
2023-04-28 22:12:35 +0800

    Update ConfigItemArgInfo.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemArgInfo.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemArgInfo.cs
@@ -1,23 +1,49 @@
-﻿using System;
-using System.Diagnostics;
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
 
 namespace Mybatis_Plus_Generator.Definition.Abstractions;
 
 public partial class ConfigItemArgInfo : ObservableObject
 {
-    [ObservableProperty] private Type argType = null!;
-    [ObservableProperty] private string argValue = string.Empty;
+    public Type ArgType
+    {
+        get => argType;
+        set
+        {
+            argType = value;
+            switch (value)
+            {
+                case { IsInterface: true }:
+                    Candidates = Assembly
+                        .GetAssembly(value)
+                        .ExportedTypes
+                        .Aggregate(new ObservableCollection<Type>(),
+                            (o, c) =>
+                            {
+                                if (!c.IsClass || !value.IsAssignableFrom(c)) return o;
+                                o.Add(c);
+                                return o;
+                            });
+                    break;
+            }
+        }
+    }
+    private Type argType = null!;
+
+    public object ArgValue
+    {
+        get => argValue;
+        set => SetProperty(ref argValue, value);
+    }
+    private object? argValue;
     [ObservableProperty] private string argName = string.Empty;
+    [ObservableProperty] private ObservableCollection<Type>? candidates;
 
     public bool IsPassword => ArgName == "password";
 
-    [RelayCommand]
-    private void OnEdit(string str)
-    {
-        Debugger.Break();
-    }
     public object TransformArg()
     {
         return argValue;
