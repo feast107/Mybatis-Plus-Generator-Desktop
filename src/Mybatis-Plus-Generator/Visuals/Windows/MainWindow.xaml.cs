3e90d080b6bbfb5d7230dc731f4d578e90fe2d4d
Feast <feast107@gmail.com>
2023-04-28 12:39:33 +0800

    Update MainWindow.xaml.cs

--- src/Mybatis-Plus-Generator/Visuals/Windows/MainWindow.xaml.cs
+++ src/Mybatis-Plus-Generator/Visuals/Windows/MainWindow.xaml.cs
@@ -6,39 +6,30 @@ using Mybatis_Plus_Generator.Extension;
 using Mybatis_Plus_Generator.ViewModels;
 using System.Windows;
 using System.Windows.Media;
-using IConfigureService =
-    Mybatis_Plus_Generator.Core.Interfaces.IConfigureService<
-        Mybatis_Plus_Generator.ViewModels.ConfigRecordViewModel,
-        Mybatis_Plus_Generator.ViewModels.ConfigInfoViewModel,
-        Mybatis_Plus_Generator.ViewModels.ConfigItemInfoViewModel>;
 
-namespace Mybatis_Plus_Generator.Visuals.Windows
+
+namespace Mybatis_Plus_Generator.Visuals.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            Core.Core.Services.AddDefaultService();
-            Core.Core.Services.AddTransient<ConfigItemInfoViewModel>();
-            Core.Core.Services.AddTransient<ConfigInfoViewModel>();
-            Core.Core.Services.AddTransient<ConfigRecordViewModel>();
-            Core.Core.Services.AddTransient<ConfigPageViewModel>();
-            Core.Core.Build();
-            var templateService = Core.Core.Provider.GetRequiredService<ITemplateService>();
-            var configureService = Core.Core.Provider.GetRequiredService<IConfigureService>();
-            var dc = Core.Core.Provider.GetRequiredService<ConfigPageViewModel>();
-            this.DataContext = dc;
-            dc.Records = configureService.Records;
-            if (configureService.Records.Count == 0)
-            {
-                dc.Current = configureService.CreateRecord("默认配置");
-            }
-            dc.Templates = templateService.AdditionalTemplates;
-            Color.FromRgb(12, 174, 135).SetTheme();
-        }
+        InitializeComponent();
+        Core.Core.Services.AddDefaultService();
+        Core.Core.Services.AddTransient<ConfigItemInfoViewModel>();
+        Core.Core.Services.AddTransient<ConfigInfoViewModel>();
+        Core.Core.Services.AddTransient<ConfigRecordViewModel>();
+        Core.Core.Services.AddTransient<ConfigPageViewModel>();
+        Core.Core.Build();
+        var templateService = Core.Core.Provider.GetRequiredService<ITemplateService>();
+        var configureService = Core.Core.Provider.GetRequiredService<IConfigureService>();
+        var dc = Core.Core.Provider.GetRequiredService<ConfigPageViewModel>();
+        this.DataContext = dc;
+        dc.Records = configureService.Records;
+        dc.Templates = templateService.AdditionalTemplates;
+        Color.FromRgb(12, 174, 135).SetTheme();
     }
 }
