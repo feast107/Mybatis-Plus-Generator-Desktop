647f2717c70e07bf4cfdfb3a0b0a362f9c9132d3
Feast <feast107@gmail.com>
2023-04-28 12:39:20 +0800

    Create Global.cs

---
+++ src/Mybatis-Plus-Generator/Global.cs
@@ -0,0 +1,10 @@
+﻿global using IConfigureService =
+    Mybatis_Plus_Generator.Core.Interfaces.IConfigureService<
+        Mybatis_Plus_Generator.ViewModels.ConfigRecordViewModel,
+        Mybatis_Plus_Generator.ViewModels.ConfigInfoViewModel,
+        Mybatis_Plus_Generator.ViewModels.ConfigItemInfoViewModel>;
+
+global using IGenerateService = Mybatis_Plus_Generator.Core.Interfaces.IGenerateService<
+    Mybatis_Plus_Generator.ViewModels.ConfigRecordViewModel,
+    Mybatis_Plus_Generator.ViewModels.ConfigInfoViewModel,
+    Mybatis_Plus_Generator.ViewModels.ConfigItemInfoViewModel>;
