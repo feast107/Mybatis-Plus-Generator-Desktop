04d335cfecce0556ec30d6f4ac639760c898cc55
Feast <feast107@gmail.com>
2023-04-28 22:13:41 +0800

    Create SimpleDialog.xaml.cs

---
+++ src/Mybatis-Plus-Generator/Visuals/Controls/Dialogs/SimpleDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Controls;
+using MaterialDesignThemes.Wpf;
+
+namespace Mybatis_Plus_Generator.Visuals.Controls.Dialogs;
+
+/// <summary>
+/// SimpleDialog.xaml 的交互逻辑
+/// </summary>
+public partial class SimpleDialog : UserControl
+{
+    public static async Task<object?> Show(string content, string host)
+    {
+        return await DialogHost.Show(new SimpleDialog(content), host);
+    }
+    public SimpleDialog(string text)
+    {
+        InitializeComponent();
+        this.Text.Tag = text;
+    }
+}
