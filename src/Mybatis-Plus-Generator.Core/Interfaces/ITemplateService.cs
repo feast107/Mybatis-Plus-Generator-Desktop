3af3794777bd37d12efd0afd6ef2cafff68a422d
Feast <feast107@gmail.com>
2023-04-28 12:38:34 +0800

    Update ITemplateService.cs

--- src/Mybatis-Plus-Generator.Core/Interfaces/ITemplateService.cs
+++ src/Mybatis-Plus-Generator.Core/Interfaces/ITemplateService.cs
@@ -1,14 +1,24 @@
 ﻿using Mybatis_Plus_Generator.Definition.Abstractions;
 using System.Collections.ObjectModel;
+using System.Reflection;
 
-namespace Mybatis_Plus_Generator.Core.Interfaces
+namespace Mybatis_Plus_Generator.Core.Interfaces;
+
+/// <summary>
+/// 模板服务
+/// </summary>
+public interface ITemplateService
 {
     /// <summary>
-    /// 模板服务
+    /// 生成器的构造函数
     /// </summary>
-    public interface ITemplateService
-    {
-        TemplateInfo PrimaryTemplate { get; }
-        ObservableCollection<TemplateInfo> AdditionalTemplates { get; }
-    }
+    ConstructorInfo GeneratorConstructor { get; }
+    /// <summary>
+    /// 首要模板信息
+    /// </summary>
+    TemplateInfo PrimaryTemplate { get; }
+    /// <summary>
+    /// 可选的模板信息
+    /// </summary>
+    ObservableCollection<TemplateInfo> AdditionalTemplates { get; }
 }
