3a2574e03cacf3c4cb096304fba27aefd8e6cf77
Feast <feast107@gmail.com>
2023-04-28 22:12:46 +0800

    Update InputTypeConverters.cs

--- src/Mybatis-Plus-Generator/Converters/InputTypeConverters.cs
+++ src/Mybatis-Plus-Generator/Converters/InputTypeConverters.cs
@@ -8,36 +8,37 @@ using Mybatis_Plus_Generator.ViewModels;
 
 namespace Mybatis_Plus_Generator.Converters;
 
-internal class InputNoArgConverter : EnumToVisibilityConverter<ConfigItemInfoViewModel.InputTypes>
+internal class InputNoArgConverter : EnumToVisibilityConverter<ConfigItemInfoViewModel.ArgTypes>
 {
-    protected override Visibility Condition(ConfigItemInfoViewModel.InputTypes condition)
+    protected override Visibility Condition(ConfigItemInfoViewModel.ArgTypes condition)
     {
         return condition switch
         {
-            ConfigItemInfoViewModel.InputTypes.NoArg => Visibility.Visible,
+            ConfigItemInfoViewModel.ArgTypes.NoArg => Visibility.Visible,
             _ => Visibility.Collapsed,
         };
     }
 }
-internal class InputStringConverter : EnumToVisibilityConverter<ConfigItemInfoViewModel.InputTypes>
+internal class InputStringConverter : EnumToVisibilityConverter<ConfigItemInfoViewModel.ArgTypes>
 {
-    protected override Visibility Condition(ConfigItemInfoViewModel.InputTypes condition)
+    protected override Visibility Condition(ConfigItemInfoViewModel.ArgTypes condition)
     {
         return condition switch
         {
-            ConfigItemInfoViewModel.InputTypes.StringArg => Visibility.Visible,
+            ConfigItemInfoViewModel.ArgTypes.SingleArg => Visibility.Visible,
             _ => Visibility.Collapsed,
         };
     }
 }
-internal class InputMultiStringConverter : EnumToVisibilityConverter<ConfigItemInfoViewModel.InputTypes>
+internal class InputMultiStringConverter : EnumToVisibilityConverter<ConfigItemInfoViewModel.ArgTypes>
 {
-    protected override Visibility Condition(ConfigItemInfoViewModel.InputTypes condition)
+    protected override Visibility Condition(ConfigItemInfoViewModel.ArgTypes condition)
     {
         return condition switch
         {
-            ConfigItemInfoViewModel.InputTypes.MultiStringArg => Visibility.Visible,
+            ConfigItemInfoViewModel.ArgTypes.MultiArg => Visibility.Visible,
             _ => Visibility.Collapsed,
         };
     }
 }
+
