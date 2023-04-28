71218c1cb3814d953e4160dd652bd6e743f83971
Feast <feast107@gmail.com>
2023-04-28 09:02:36 +0800

    Create InputTypeConverters.cs

---
+++ src/Mybatis-Plus-Generator/Converters/InputTypeConverters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Mybatis_Plus_Generator.ViewModels;
+
+namespace Mybatis_Plus_Generator.Converters
+{
+    internal class InputNoArgConverter : EnumToVisibilityConverter<ConfigItemInfoViewModel.InputTypes>
+    {
+        protected override Visibility Condition(ConfigItemInfoViewModel.InputTypes condition)
+        {
+            return condition switch
+            {
+                ConfigItemInfoViewModel.InputTypes.NoArg => Visibility.Visible,
+                _ => Visibility.Collapsed,
+            };
+        }
+    }
+    internal class InputStringConverter : EnumToVisibilityConverter<ConfigItemInfoViewModel.InputTypes>
+    {
+        protected override Visibility Condition(ConfigItemInfoViewModel.InputTypes condition)
+        {
+            return condition switch
+            {
+                ConfigItemInfoViewModel.InputTypes.StringArg => Visibility.Visible,
+                _ => Visibility.Collapsed,
+            };
+        }
+    }
+    internal class InputMultiStringConverter : EnumToVisibilityConverter<ConfigItemInfoViewModel.InputTypes>
+    {
+        protected override Visibility Condition(ConfigItemInfoViewModel.InputTypes condition)
+        {
+            return condition switch
+            {
+                ConfigItemInfoViewModel.InputTypes.MultiStringArg => Visibility.Visible,
+                _ => Visibility.Collapsed,
+            };
+        }
+    }
+}
