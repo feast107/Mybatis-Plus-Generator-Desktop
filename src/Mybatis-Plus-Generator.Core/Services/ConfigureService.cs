4cacf1e0e228bc86fa05085129bea80a5bf27816
Feast <feast107@gmail.com>
2023-04-28 12:38:47 +0800

    Update ConfigureService.cs

--- src/Mybatis-Plus-Generator.Core/Services/ConfigureService.cs
+++ src/Mybatis-Plus-Generator.Core/Services/ConfigureService.cs
@@ -4,53 +4,67 @@ using Microsoft.Extensions.DependencyInjection;
 using Mybatis_Plus_Generator.Core.Interfaces;
 using Mybatis_Plus_Generator.Definition.Abstractions;
 
-namespace Mybatis_Plus_Generator.Core.Services
+namespace Mybatis_Plus_Generator.Core.Services;
+
+internal class ConfigureService<TConfigRecord,TConfigInfo, TConfigItemInfo> 
+    : IConfigureService<TConfigRecord,TConfigInfo, TConfigItemInfo>
+    where TConfigRecord : ConfigRecord<TConfigInfo, TConfigItemInfo>
+    where TConfigItemInfo : ConfigItemInfo
+    where TConfigInfo : ConfigInfo<TConfigItemInfo>
 {
-    internal class ConfigureService<TConfigRecord,TConfigInfo, TConfigItemInfo> 
-        : IConfigureService<TConfigRecord,TConfigInfo, TConfigItemInfo>
-        where TConfigRecord : ConfigRecord<TConfigInfo, TConfigItemInfo>
-        where TConfigItemInfo : ConfigItemInfo
-        where TConfigInfo : ConfigInfo<TConfigItemInfo>
+    private readonly ITemplateService templateService;
+    public ConfigureService(ITemplateService templateService)
     {
-        private readonly ITemplateService templateService;
-        public ConfigureService(ITemplateService templateService)
-        {
-            this.templateService = templateService;
-        }
+        this.templateService = templateService;
+    }
 
-        public ObservableCollection<TConfigRecord> Records { get; } = new ();
+    public object?[]? ResolveArgs(TConfigItemInfo configItemInfo)
+    {
+        var index = 0;
+        return !configItemInfo.HasParam
+            ? null
+            : configItemInfo.SelectMethod
+                .GetParameters()
+                .Aggregate(new List<object>(), (l, p) =>
+                {
+                    l.Add(configItemInfo.Args[index++].TransformArg());
+                    return l;
+                })
+                .ToArray();
+    }
 
-        public TConfigRecord CreateRecord(string name)
+    public ObservableCollection<TConfigRecord> Records { get; } = new ();
+
+    public TConfigRecord CreateRecord(string name)
+    {
+        var record = Records.FirstOrDefault(x => x.ConfigName == name);
+        if(record != null)
         {
-            var record = Records.FirstOrDefault(x => x.ConfigName == name);
-            if(record != null)
-            {
-                return record;
-            }
-            var pt = templateService.PrimaryTemplate;
-            var tmp = Instantiate(pt, pt.Name ?? "");
-            record = Core.Provider.GetRequiredService<TConfigRecord>();
-            record.ConfigName = name;
-            record.FixedConfig = tmp;
-            record.Configs = new ObservableCollection<TConfigInfo>() { tmp };
-            Records.Add(record);
             return record;
         }
+        var pt = templateService.PrimaryTemplate;
+        var tmp = Instantiate(pt, pt.Name ?? "");
+        record = Core.Provider.GetRequiredService<TConfigRecord>();
+        record.ConfigName = name;
+        record.FixedConfig = tmp;
+        record.Configs = new ObservableCollection<TConfigInfo>() { tmp };
+        Records.Add(record);
+        return record;
+    }
 
-        public TConfigInfo Instantiate(TemplateInfo template, string name)
+    public TConfigInfo Instantiate(TemplateInfo template, string name)
+    {
+        var config = Core.Provider.GetRequiredService<TConfigInfo>();
+        config.TemplateInfo = template;
+        config.ConfigName = name;
+        config.ConfigItems = template.Fields.Aggregate(new ObservableCollection<TConfigItemInfo>(), (o, e) =>
         {
-            var config = Core.Provider.GetRequiredService<TConfigInfo>();
-            config.TemplateInfo = template;
-            config.ConfigName = name;
-            config.ConfigItems = template.Fields.Aggregate(new ObservableCollection<TConfigItemInfo>(), (o, e) =>
-            {
-                var info = Core.Provider.GetRequiredService<TConfigItemInfo>();
-                info.TemplateInfo = e;
-                info.SelectMethod = e.Methods[0];
-                o.Add(info);
-                return o;
-            });
-            return config;
-        }
+            var info = Core.Provider.GetRequiredService<TConfigItemInfo>();
+            info.TemplateInfo = e;
+            info.SelectMethod = e.Methods[0];
+            o.Add(info);
+            return o;
+        });
+        return config;
     }
 }
