19815c8282d6d557071f5acd9e728d8ff0dc6f61
Feast <feast107@gmail.com>
2023-04-28 09:02:46 +0800

    Update ConfigItemInfoViewModel.cs

--- src/Mybatis-Plus-Generator/ViewModels/ConfigItemInfoViewModel.cs
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigItemInfoViewModel.cs
@@ -5,16 +5,33 @@ namespace Mybatis_Plus_Generator.ViewModels
 {
     internal class ConfigItemInfoViewModel : ConfigItemInfo
     {
+        public enum InputTypes
+        {
+            NoArg,
+            StringArg,
+            MultiStringArg
+        }
+
+        public InputTypes InputType => SelectMethod.GetParameters().Length switch
+        {
+            0 => InputTypes.NoArg,
+            1 => InputTypes.StringArg,
+            _ => InputTypes.MultiStringArg
+        };
+
         /// <summary>
         /// 可选
         /// </summary>
-        public bool CanSwitch => CanIgnore || !TemplateInfo!.IsCtor;
+        public bool ShowSwitch => 
+            !TemplateInfo!.IsCtor || CanIgnore;
         /// <summary>
         /// 可以忽略
         /// </summary>
-        public bool CanIgnore =>
-            canIgnore ??= TemplateInfo!.IsCtor && TemplateInfo.Methods.All(x => x.GetParameters().Length == 0);
+        public bool CanIgnore => canIgnore ??= 
+            TemplateInfo!.IsCtor && TemplateInfo.Methods.Any(x => x.GetParameters().Length == 0);
+
         private bool? canIgnore;
+
         public SwitchCondition Condition => TemplateInfo!.AllowMultiple
             ? IsGenerated
                 ? SwitchCondition.ShowRemove
