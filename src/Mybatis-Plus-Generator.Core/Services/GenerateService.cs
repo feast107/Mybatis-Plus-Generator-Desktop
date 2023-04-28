69a3cbc775e20a042fdc1bc1dfa379d502f8f264
Feast <feast107@gmail.com>
2023-04-28 10:24:46 +0800

    Create GenerateService.cs

---
+++ src/Mybatis-Plus-Generator.Core/Services/GenerateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mybatis_Plus_Generator.Core.Interfaces;
+
+namespace Mybatis_Plus_Generator.Core.Services
+{
+    internal class GenerateService : IGenerateService
+    {
+    }
+}
