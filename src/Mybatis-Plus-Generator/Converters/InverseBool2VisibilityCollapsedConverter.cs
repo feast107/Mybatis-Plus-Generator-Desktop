6e5cbd7ad54eff21e50611cc85ff86a25b0e3f18
Feast <feast107@gmail.com>
2023-04-28 12:38:58 +0800

    Update InverseBool2VisibilityCollapsedConverter.cs

--- src/Mybatis-Plus-Generator/Converters/InverseBool2VisibilityCollapsedConverter.cs
+++ src/Mybatis-Plus-Generator/Converters/InverseBool2VisibilityCollapsedConverter.cs
@@ -3,18 +3,17 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace Mybatis_Plus_Generator.Converters
-{
-    internal class InverseBool2VisibilityCollapsedConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value is true ? Visibility.Collapsed : Visibility.Visible;
-        }
+namespace Mybatis_Plus_Generator.Converters;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value is not Visibility.Visible;
-        }
+internal class InverseBool2VisibilityCollapsedConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is true ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is not Visibility.Visible;
     }
 }
