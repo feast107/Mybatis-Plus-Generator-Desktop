76f1cf8517664ce3ab4c87b722d33c1cb75afb3c
Feast <feast107@gmail.com>
2023-04-28 15:31:21 +0800

    Create TypeDescribeConverter.cs

---
+++ src/Mybatis-Plus-Generator/Converters/TypeDescribeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Mybatis_Plus_Generator.Converters
+{
+    internal class TypeDescribeConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new LangConverter().Convert(value, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
