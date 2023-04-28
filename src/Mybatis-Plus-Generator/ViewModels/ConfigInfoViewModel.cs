d470cf1e6ec8ca7597d036e22af2f24f6124fc69
Feast <feast107@gmail.com>
2023-04-28 12:39:19 +0800

    Update ConfigInfoViewModel.cs

--- src/Mybatis-Plus-Generator/ViewModels/ConfigInfoViewModel.cs
+++ src/Mybatis-Plus-Generator/ViewModels/ConfigInfoViewModel.cs
@@ -1,49 +1,43 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using Mybatis_Plus_Generator.Definition.Abstractions;
 using System.Linq;
-using IConfigureService = 
-    Mybatis_Plus_Generator.Core.Interfaces.IConfigureService<
-    Mybatis_Plus_Generator.ViewModels.ConfigRecordViewModel,
-    Mybatis_Plus_Generator.ViewModels.ConfigInfoViewModel,
-    Mybatis_Plus_Generator.ViewModels.ConfigItemInfoViewModel>;
 
-namespace Mybatis_Plus_Generator.ViewModels
+namespace Mybatis_Plus_Generator.ViewModels;
+
+internal partial class ConfigInfoViewModel : ConfigInfo<ConfigItemInfoViewModel>
 {
-    internal partial class ConfigInfoViewModel : ConfigInfo<ConfigItemInfoViewModel>
+    private readonly IConfigureService configureService;
+
+    public ConfigInfoViewModel(IConfigureService configureService)
     {
-        private readonly IConfigureService configureService;
+        this.configureService = configureService;
+    }
 
-        public ConfigInfoViewModel(IConfigureService configureService)
+    public bool IgnoreCtor => ignoreCtor ??= TemplateInfo!
+        .Fields
+        .Where(x => x.IsCtor)
+        .All(x => x.Methods.All(c => c.GetParameters().Length == 0));
+    private bool? ignoreCtor;
+
+    [RelayCommand]
+    void AddConfig(TemplateItemInfo templateItem)
+    {
+        for (var i = 0; i < base.ConfigItems.Count; i++)
         {
-            this.configureService = configureService;
-        }
-
-        public bool IgnoreCtor => ignoreCtor ??= TemplateInfo!
-            .Fields
-            .Where(x => x.IsCtor)
-            .All(x => x.Methods.All(c => c.GetParameters().Length == 0));
-        private bool? ignoreCtor;
-
-        [RelayCommand]
-        void AddConfig(TemplateItemInfo templateItem)
-        {
-            for (var i = 0; i < base.ConfigItems.Count; i++)
+            if (ConfigItems[i].TemplateInfo == templateItem)
             {
-                if (ConfigItems[i].TemplateInfo == templateItem)
+                ConfigItems.Insert(i + 1, new ConfigItemInfoViewModel()
                 {
-                    ConfigItems.Insert(i + 1, new ConfigItemInfoViewModel()
-                    {
-                        TemplateInfo = templateItem,
-                        IsGenerated = true,
-                    });
-                    return;
-                }
+                    TemplateInfo = templateItem,
+                    IsGenerated = true,
+                });
+                return;
             }
         }
-        [RelayCommand]
-        void RemoveConfig(ConfigItemInfoViewModel configInfo)
-        {
-            ConfigItems.Remove(configInfo);
-        }
+    }
+    [RelayCommand]
+    void RemoveConfig(ConfigItemInfoViewModel configInfo)
+    {
+        ConfigItems.Remove(configInfo);
     }
 }
