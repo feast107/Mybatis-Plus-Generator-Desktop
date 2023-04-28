7b135cb0ef88732f71290a892600f1ee25896f7c
Feast <feast107@gmail.com>
2023-04-28 15:31:31 +0800

    Update TemplateInfo.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/TemplateInfo.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/TemplateInfo.cs
@@ -9,7 +9,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 namespace Mybatis_Plus_Generator.Definition.Abstractions;
 
 /// <summary>
-/// 配置模板
+/// 配置模板信息
 /// </summary>
 public partial class TemplateInfo : ObservableObject
 {
@@ -18,11 +18,11 @@ public partial class TemplateInfo : ObservableObject
     /// </summary>
     [ObservableProperty] private string name = string.Empty;
     /// <summary>
-    /// 模板类型
+    /// 配置类型
     /// </summary>
     [ObservableProperty] private Type configType = null!;
     /// <summary>
-    /// 配置函数
+    /// 配置项信息
     /// </summary>
     [ObservableProperty] private ObservableCollection<TemplateItemInfo> fields = new();
 }
