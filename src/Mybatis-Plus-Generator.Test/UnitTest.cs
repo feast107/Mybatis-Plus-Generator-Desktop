290de7d85ac9d7ad59f0f12223e579739f6f29f2
Feast <feast107@gmail.com>
2023-04-28 22:12:34 +0800

    Update UnitTest.cs

--- src/Mybatis-Plus-Generator.Test/UnitTest.cs
+++ src/Mybatis-Plus-Generator.Test/UnitTest.cs
@@ -3,34 +3,34 @@ using Mybatis_Plus_Generator.Core.Extensions;
 using Mybatis_Plus_Generator.Core.Interfaces;
 using Mybatis_Plus_Generator.Extension;
 
-namespace Mybatis_Plus_Generator.Test
+namespace Mybatis_Plus_Generator.Test;
+
+public class Tests
 {
-    public class Tests
+    private ITemplateService TemplateService { get; set; }
+    private IConfigureService ConfigureService { get; set; }
+    private IGenerateService GenerateService { get; set; }
+    [SetUp]
+    public void Setup()
     {
-        private ITemplateService TemplateService { get; set; }
-        private IConfigureService ConfigureService { get; set; }
-        private IGenerateService GenerateService { get; set; }
-        [SetUp]
-        public void Setup()
-        {
-            Core.Core.Services.AddDefaultService();
-            Core.Core.Services.AddViewModels();
-            Core.Core.Build();
-            TemplateService = Core.Core.Provider.GetRequiredService<ITemplateService>();
-            ConfigureService = Core.Core.Provider.GetRequiredService<IConfigureService>();
-            GenerateService = Core.Core.Provider.GetRequiredService<IGenerateService>();
-        }
+        Core.Core.Services.AddDefaultService();
+        Core.Core.Services.AddViewModels();
+        Core.Core.Build();
+        TemplateService = Core.Core.Provider.GetRequiredService<ITemplateService>();
+        ConfigureService = Core.Core.Provider.GetRequiredService<IConfigureService>();
+        GenerateService = Core.Core.Provider.GetRequiredService<IGenerateService>();
+    }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.NotNull(GenerateService);
-        }
+    [Test]
+    public void Test1()
+    {
+        Assert.NotNull(GenerateService);
+        
+    }
 
-        [Test]
-        public void AnotherTest()
-        {
-            Assert.NotNull(ConfigureService);
-        }
+    [Test]
+    public void AnotherTest()
+    {
+        Assert.NotNull(ConfigureService);
     }
 }
