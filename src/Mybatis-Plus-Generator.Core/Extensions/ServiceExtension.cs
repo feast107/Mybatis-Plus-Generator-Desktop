3f09b6953e84c458a419d720e3c77b52855c3249
Feast <feast107@gmail.com>
2023-04-28 12:39:31 +0800

    Update ServiceExtension.cs

--- src/Mybatis-Plus-Generator.Core/Extensions/ServiceExtension.cs
+++ src/Mybatis-Plus-Generator.Core/Extensions/ServiceExtension.cs
@@ -2,16 +2,13 @@
 using Mybatis_Plus_Generator.Core.Interfaces;
 using Mybatis_Plus_Generator.Core.Services;
 
-namespace Mybatis_Plus_Generator.Core.Extensions
-{
-    public static class ServiceExtension
-    {
-        public static IServiceCollection AddDefaultService(this IServiceCollection collection) 
-            => collection.AddSingleton<ITemplateService, TemplateService>()
-                .AddSingleton(typeof(IConfigureService<,,>),typeof(ConfigureService<,,>));
+namespace Mybatis_Plus_Generator.Core.Extensions;
 
-        public static IServiceCollection AddDefaultService(this IServiceCollection collection,Type configServiceType)
-            => collection.AddSingleton<ITemplateService, TemplateService>()
-                .AddSingleton(configServiceType, typeof(ConfigureService<,,>));
-    }
+public static class ServiceExtension
+{
+    public static IServiceCollection AddDefaultService(this IServiceCollection collection)
+        => collection
+            .AddSingleton<ITemplateService, TemplateService>()
+            .AddSingleton(typeof(IConfigureService<,,>), typeof(ConfigureService<,,>))
+            .AddSingleton(typeof(IGenerateService<,,>), typeof(GenerateService<,,>));
 }
