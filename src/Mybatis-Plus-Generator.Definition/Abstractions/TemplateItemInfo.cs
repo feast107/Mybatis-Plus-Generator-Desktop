de58249db1c4380f6759a3be9e273c9cf5e72b8f
Feast <feast107@gmail.com>
2023-04-28 12:38:54 +0800

    Update TemplateItemInfo.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/TemplateItemInfo.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/TemplateItemInfo.cs
@@ -2,25 +2,24 @@
 using System.Collections.ObjectModel;
 using System.Reflection;
 
-namespace Mybatis_Plus_Generator.Definition.Abstractions
+namespace Mybatis_Plus_Generator.Definition.Abstractions;
+
+public partial class TemplateItemInfo : ObservableObject
 {
-    public partial class TemplateItemInfo : ObservableObject
-    {
-        /// <summary>
-        /// 配置项名称
-        /// </summary>
-        [ObservableProperty] private string? fieldName;
-        /// <summary>
-        /// 是否允许多个
-        /// </summary>
-        [ObservableProperty] bool allowMultiple;
-        /// <summary>
-        /// 调用方法
-        /// </summary>
-        [ObservableProperty] private ObservableCollection<MethodBase> methods = new();
-        /// <summary>
-        /// 是否是构造函数
-        /// </summary>
-        [ObservableProperty] private bool isCtor;
-    }
+    /// <summary>
+    /// 配置项名称
+    /// </summary>
+    [ObservableProperty] private string fieldName = string.Empty;
+    /// <summary>
+    /// 是否允许多个
+    /// </summary>
+    [ObservableProperty] private bool allowMultiple;
+    /// <summary>
+    /// 调用方法
+    /// </summary>
+    [ObservableProperty] private ObservableCollection<MethodBase> methods = new();
+    /// <summary>
+    /// 是否是构造函数
+    /// </summary>
+    [ObservableProperty] private bool isCtor;
 }
