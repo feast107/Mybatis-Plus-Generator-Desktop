fd0cbd36d511eada00f1197f8d686b5418dd6213
Feast <feast107@gmail.com>
2023-04-29 01:24:45 +0800

    Update ConfigItemInfo.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemInfo.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemInfo.cs
@@ -14,6 +14,34 @@ public partial class ConfigItemInfo : ObservableObject
         None
     }
 
+    public ConfigItemInfo()
+    {
+        PropertyChanged += (_, e) =>
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(TemplateItemInfo):
+                    SelectMethod = TemplateItemInfo.Methods[0];
+                    break;
+                case nameof(SelectMethod):
+                    Args = SelectMethod
+                        .GetParameters()
+                        .Aggregate(new ObservableCollection<ConfigItemArgInfo>(),
+                            (o, c) =>
+                        {
+                            o.Add(new ConfigItemArgInfo
+                            {
+                                ArgName = c.Name,
+                                ArgType = c.ParameterType,
+                            });
+                            return o;
+                        });
+                    OnPropertyChanged(nameof(HasParam));
+                    break;
+            }
+        };
+    }
+
     public string FieldName => TemplateItemInfo.FieldName;
     /// <summary>
     /// 关联模板
@@ -22,29 +50,7 @@ public partial class ConfigItemInfo : ObservableObject
     [ObservableProperty] private bool isEnable = true;
     [ObservableProperty] private bool isGenerated;
     [ObservableProperty] private ObservableCollection<ConfigItemArgInfo> args = null!;
+    [ObservableProperty] private MethodBase selectMethod = null!;
 
     public bool HasParam => SelectMethod.GetParameters().Length != 0;
-    public MethodBase SelectMethod
-    {
-        get => selectMethod ??= ChangeSelectMethod(TemplateItemInfo!.Methods[0]);
-        set => ChangeSelectMethod(value);
-    }
-    private MethodBase? selectMethod;
-    private MethodBase ChangeSelectMethod(MethodBase method)
-    {
-        SetProperty(ref selectMethod, method);
-        var param = method.GetParameters();
-        Args = param.Aggregate(new ObservableCollection<ConfigItemArgInfo>(), (o, c) =>
-        {
-            o.Add(new ConfigItemArgInfo()
-            {
-                ArgName = c.Name,
-                ArgType = c.ParameterType,
-            });
-            return o;
-        });
-        OnPropertyChanged(nameof(HasParam));
-        return method;
-    }
-
 }
