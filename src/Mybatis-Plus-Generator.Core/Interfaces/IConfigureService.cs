a79529b930e01fd97c7c833958433828cdb6c3ad
Feast <feast107@gmail.com>
2023-04-28 12:38:37 +0800

    Update IConfigureService.cs

--- src/Mybatis-Plus-Generator.Core/Interfaces/IConfigureService.cs
+++ src/Mybatis-Plus-Generator.Core/Interfaces/IConfigureService.cs
@@ -1,15 +1,34 @@
 ﻿using System.Collections.ObjectModel;
 using Mybatis_Plus_Generator.Definition.Abstractions;
 
-namespace Mybatis_Plus_Generator.Core.Interfaces
+namespace Mybatis_Plus_Generator.Core.Interfaces;
+
+public interface IConfigureService<TConfigRecord,out TConfigInfo,in TConfigItemInfo>
+    where TConfigRecord : ConfigRecord<TConfigInfo, TConfigItemInfo>
+    where TConfigInfo : ConfigInfo<TConfigItemInfo>
+    where TConfigItemInfo : ConfigItemInfo
 {
-    public interface IConfigureService<TConfigRecord, out TConfigInfo, TConfigItemInfo>
-        where TConfigRecord : ConfigRecord<TConfigInfo, TConfigItemInfo>
-        where TConfigInfo : ConfigInfo<TConfigItemInfo>
-        where TConfigItemInfo : ConfigItemInfo
-    {
-        ObservableCollection<TConfigRecord> Records { get; }
-        TConfigRecord CreateRecord(string name);
-        TConfigInfo Instantiate(TemplateInfo template, string name);
-    }
+    /// <summary>
+    /// 解析参数
+    /// </summary>
+    /// <param name="configInfo">配置信息</param>
+    /// <returns></returns>
+    object?[]? ResolveArgs(TConfigItemInfo configInfo);
+    /// <summary>
+    /// 所有的配置记录
+    /// </summary>
+    ObservableCollection<TConfigRecord> Records { get; }
+    /// <summary>
+    /// 创建一个配置
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    TConfigRecord CreateRecord(string name);
+    /// <summary>
+    /// 实例化一个模板
+    /// </summary>
+    /// <param name="template"></param>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    TConfigInfo Instantiate(TemplateInfo template, string name);
 }
