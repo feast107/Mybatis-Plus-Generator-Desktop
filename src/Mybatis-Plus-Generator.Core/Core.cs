997cd002f2f852f6c5a68757b34246101545ab44
Feast <feast107@gmail.com>
2023-04-28 12:38:32 +0800

    Update Core.cs

--- src/Mybatis-Plus-Generator.Core/Core.cs
+++ src/Mybatis-Plus-Generator.Core/Core.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
-namespace Mybatis_Plus_Generator.Core
+namespace Mybatis_Plus_Generator.Core;
+
+public static class Core
 {
-    public static class Core
+    public static IServiceCollection Services { get; } = new ServiceCollection();
+
+    private static ServiceProvider? provider;
+    public static IServiceProvider Provider => provider ?? throw new Exception("Service hasn't been build");
+
+    public static IServiceProvider Build()
     {
-        public static IServiceCollection Services { get; } = new ServiceCollection();
-
-        private static ServiceProvider? provider;
-        public static IServiceProvider Provider => provider ?? throw new Exception("Service hasn't been build");
-
-        public static IServiceProvider Build()
-        {
-            provider?.Dispose();
-            provider = Services.BuildServiceProvider();
-            return provider;
-        }
+        provider?.Dispose();
+        provider = Services.BuildServiceProvider();
+        return provider;
     }
 }
