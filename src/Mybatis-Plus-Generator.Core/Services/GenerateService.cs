dd21f973ed35ba321c996cd766bf5cac8e6eef61
Feast <feast107@gmail.com>
2023-04-28 12:38:33 +0800

    Update GenerateService.cs

--- src/Mybatis-Plus-Generator.Core/Services/GenerateService.cs
+++ src/Mybatis-Plus-Generator.Core/Services/GenerateService.cs
@@ -1,13 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using com.baomidou.mybatisplus.generator;
+using ikvm.extensions;
 using Mybatis_Plus_Generator.Core.Interfaces;
+using Mybatis_Plus_Generator.Definition.Abstractions;
 
-namespace Mybatis_Plus_Generator.Core.Services
+namespace Mybatis_Plus_Generator.Core.Services;
+
+internal class GenerateService<TConfigRecord, TConfigInfo, TConfigItemInfo>
+    : IGenerateService<TConfigRecord, TConfigInfo, TConfigItemInfo>
+    where TConfigRecord : ConfigRecord<TConfigInfo, TConfigItemInfo>
+    where TConfigInfo : ConfigInfo<TConfigItemInfo>
+    where TConfigItemInfo : ConfigItemInfo
 {
-    internal class GenerateService : IGenerateService
+    private readonly ITemplateService templateService;
+    private readonly IConfigureService<TConfigRecord,TConfigInfo,TConfigItemInfo> configureService;
+
+    public GenerateService(ITemplateService templateService, 
+        IConfigureService<TConfigRecord, TConfigInfo, TConfigItemInfo> configureService)
     {
+        this.templateService = templateService;
+        this.configureService = configureService;
+    }
+
+    public Task<bool> Generate(TConfigRecord record)
+    {
+        throw new NotImplementedException();
+        record.Configs.Aggregate(
+            templateService.GeneratorConstructor.Invoke( new []{Config(record.FixedConfig!)}), (o, e) => o);
+    }
+
+    private object Config(TConfigInfo configInfo)
+    {
+        var builder = configInfo.ConfigItems.FirstOrDefault(x => x.TemplateInfo.IsCtor);
+        object config;
+        if (builder == null)
+        {
+            var ctorArgs = builder!.Args.Select(x=>(object)x).ToArray();
+            config = builder.SelectMethod.Invoke(null, ctorArgs)!;
+        }
+        else
+        {
+            config = Activator.CreateInstance(configInfo.TemplateInfo!.ConfigType!)!;
+        }
+        return Config(config, configInfo);
+    }
+
+    private object Config(object config, TConfigInfo configInfo)
+    {
+        return configInfo.ConfigItems.Aggregate(config,Config);
+    }
+
+    private object Config(object config, ConfigItemInfo item)
+    {
+        if (!item.IsEnable) return config;
+        item.SelectMethod.Invoke(config, item.SelectMethod.GetParameters().Length == 0
+            ? null
+            : item.Args!
+                .Select(x => (object?)x.ArgValue)
+                .ToArray());
+        return config;
     }
 }
