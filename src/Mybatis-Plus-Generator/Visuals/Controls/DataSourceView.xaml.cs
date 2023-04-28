23fe0a9e87091c411c39f8fed0f46b8b58f6dd46
Feast <feast107@gmail.com>
2023-04-28 12:39:28 +0800

    Update DataSourceView.xaml.cs

--- src/Mybatis-Plus-Generator/Visuals/Controls/DataSourceView.xaml.cs
+++ src/Mybatis-Plus-Generator/Visuals/Controls/DataSourceView.xaml.cs
@@ -1,15 +1,14 @@
 ﻿using System.Windows.Controls;
 
-namespace Mybatis_Plus_Generator.Visuals.Controls
+namespace Mybatis_Plus_Generator.Visuals.Controls;
+
+/// <summary>
+/// DataSourcePage.xaml 的交互逻辑
+/// </summary>
+public partial class DataSourcePage : UserControl
 {
-    /// <summary>
-    /// DataSourcePage.xaml 的交互逻辑
-    /// </summary>
-    public partial class DataSourcePage : UserControl
+    public DataSourcePage()
     {
-        public DataSourcePage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
