ecb14393bebd0d62b7ffb4dd6eb66714a4ab6e2d
Feast <feast107@gmail.com>
2023-04-28 10:24:34 +0800

    Create IGenerateService.cs

---
+++ src/Mybatis-Plus-Generator.Core/Interfaces/IGenerateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mybatis_Plus_Generator.Core.Interfaces
+{
+    public interface IGenerateService
+    {
+    }
+}
