16bf8c5ce9ff60f7a9ba2f606e09cdf58ca9bf73
Feast <feast107@gmail.com>
2023-04-28 22:13:17 +0800

    Create TextArg.xaml.cs

---
+++ src/Mybatis-Plus-Generator/Visuals/Controls/Args/TextArg.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Mybatis_Plus_Generator.Visuals.Controls.Args;
+
+/// <summary>
+/// TextInput.xaml 的交互逻辑
+/// </summary>
+public partial class TextArg : UserControl
+{
+    public TextArg()
+    {
+        InitializeComponent();
+    }
+}
