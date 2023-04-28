30d46e6f219d1870260fb4c03f5a7384f98f668c
Feast <feast107@gmail.com>
2023-04-28 12:38:55 +0800

    Update IndexConverter.cs

--- src/Mybatis-Plus-Generator/Converters/IndexConverter.cs
+++ src/Mybatis-Plus-Generator/Converters/IndexConverter.cs
@@ -3,18 +3,17 @@ using System.Collections;
 using System.Globalization;
 using System.Windows.Data;
 
-namespace Mybatis_Plus_Generator.Converters
-{
-    public class IndexConverter : IValueConverter
-    {
-        public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return ((int)value -1);
-        }
+namespace Mybatis_Plus_Generator.Converters;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
+public class IndexConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return ((int)value -1);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
     }
 }
