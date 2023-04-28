a4b4164d62ca3858a4703d9ff1938bc207126014
Feast <feast107@gmail.com>
2023-04-28 22:13:06 +0800

    Create PasswordArg.xaml.cs

---
+++ src/Mybatis-Plus-Generator/Visuals/Controls/Args/PasswordArg.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Mybatis_Plus_Generator.Visuals.Controls.Args;
+
+/// <summary>
+/// PasswordInput.xaml 的交互逻辑
+/// </summary>
+public partial class PasswordArg : UserControl
+{
+    public PasswordArg()
+    {
+        InitializeComponent();
+    }
+}
