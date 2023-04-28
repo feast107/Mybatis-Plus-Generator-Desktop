6bba4fdae21028d3daa3de2aa39c0aab679ba6c3
Feast <feast107@gmail.com>
2023-04-28 12:38:39 +0800

    Update ConfigInfo.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigInfo.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigInfo.cs
@@ -1,21 +1,23 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
-namespace Mybatis_Plus_Generator.Definition.Abstractions
+namespace Mybatis_Plus_Generator.Definition.Abstractions;
+
+public partial class ConfigInfo<TConfigItemInfo> : ObservableObject where TConfigItemInfo : ConfigItemInfo
 {
-    public partial class ConfigInfo<TConfigItemInfo> : ObservableObject where TConfigItemInfo : ConfigItemInfo
-    {
-        /// <summary>
-        /// 关联模板
-        /// </summary>
-        [ObservableProperty] private TemplateInfo? templateInfo;
-        /// <summary>
-        /// 配置项
-        /// </summary>
-        [ObservableProperty] private ObservableCollection<TConfigItemInfo> configItems = new();
-        /// <summary>
-        /// 配置名称
-        /// </summary>
-        [ObservableProperty] private string? configName;
-    }
+    /// <summary>
+    /// 关联模板
+    /// </summary>
+    [ObservableProperty] private TemplateInfo templateInfo = null!;
+
+    /// <summary>
+    /// 配置项
+    /// </summary>
+    [ObservableProperty] private ObservableCollection<TConfigItemInfo> configItems = new();
+
+    /// <summary>
+    /// 配置名称
+    /// </summary>
+    [ObservableProperty] private string configName = null!;
 }
