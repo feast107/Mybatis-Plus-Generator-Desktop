4181ff83e5e677c027a6aac7900e13a0b435c4f1
Feast <feast107@gmail.com>
2023-04-28 12:38:46 +0800

    Update CompileAttribute.cs

--- src/Mybatis-Plus-Generator.Definition/Attributes/CompileAttribute.cs
+++ src/Mybatis-Plus-Generator.Definition/Attributes/CompileAttribute.cs
@@ -3,27 +3,26 @@ using System;
 using System.Collections.Generic;
 
 // ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+public sealed class CompilerFeatureRequiredAttribute : Attribute
 {
-    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
-    public sealed class CompilerFeatureRequiredAttribute : Attribute
+    public const string RefStructs = "RefStructs";
+
+    public const string RequiredMembers = "RequiredMembers";
+
+    public string FeatureName { get; }
+
+    public bool IsOptional { get; init; }
+
+    public CompilerFeatureRequiredAttribute(string featureName)
     {
-        public const string RefStructs = "RefStructs";
-
-        public const string RequiredMembers = "RequiredMembers";
-
-        public string FeatureName { get; }
-
-        public bool IsOptional { get; init; }
-
-        public CompilerFeatureRequiredAttribute(string featureName)
-        {
-            FeatureName = featureName;
-        }
+        FeatureName = featureName;
     }
-
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
-    public sealed class RequiredMemberAttribute : Attribute { }
-
-    internal sealed class IsExternalInit{}
 }
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+public sealed class RequiredMemberAttribute : Attribute { }
+
+internal sealed class IsExternalInit{}
