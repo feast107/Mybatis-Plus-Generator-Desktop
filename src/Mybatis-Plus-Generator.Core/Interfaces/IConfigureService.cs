fa9e841eab2bfef49eeff346d04b399b7662ea6b
Feast <feast107@gmail.com>
2023-04-28 15:31:00 +0800

    Update IConfigureService.cs

--- src/Mybatis-Plus-Generator.Core/Interfaces/IConfigureService.cs
+++ src/Mybatis-Plus-Generator.Core/Interfaces/IConfigureService.cs
@@ -3,17 +3,11 @@ using Mybatis_Plus_Generator.Definition.Abstractions;
 
 namespace Mybatis_Plus_Generator.Core.Interfaces;
 
-public interface IConfigureService<TConfigRecord,out TConfigInfo,in TConfigItemInfo>
+public interface IConfigureService<TConfigRecord,TConfigInfo,in TConfigItemInfo>
     where TConfigRecord : ConfigRecord<TConfigInfo, TConfigItemInfo>
     where TConfigInfo : ConfigInfo<TConfigItemInfo>
     where TConfigItemInfo : ConfigItemInfo
 {
-    /// <summary>
-    /// 解析参数
-    /// </summary>
-    /// <param name="configInfo">配置信息</param>
-    /// <returns></returns>
-    object?[]? ResolveArgs(TConfigItemInfo configInfo);
     /// <summary>
     /// 所有的配置记录
     /// </summary>
@@ -31,4 +25,18 @@ public interface IConfigureService<TConfigRecord,out TConfigInfo,in TConfigItemI
     /// <param name="name"></param>
     /// <returns></returns>
     TConfigInfo Instantiate(TemplateInfo template, string name);
+    /// <summary>
+    /// 解析参数
+    /// </summary>
+    /// <param name="configInfo">配置信息</param>
+    /// <returns></returns>
+    object?[]? ResolveArgs(TConfigItemInfo configInfo);
+
+    /// <summary>
+    /// 应用配置
+    /// </summary>
+    /// <param name="config"></param>
+    /// <param name="info"></param>
+    /// <returns></returns>
+    object Apply(object config, TConfigInfo info);
 }
