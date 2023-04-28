0a958a0fe1af17f7b68d2f4af496100c190fa4d2
Feast <feast107@gmail.com>
2023-04-28 12:38:41 +0800

    Update ConfigItemArgInfo.cs

--- src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemArgInfo.cs
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemArgInfo.cs
@@ -5,7 +5,12 @@ namespace Mybatis_Plus_Generator.Definition.Abstractions;
 
 public partial class ConfigItemArgInfo : ObservableObject
 {
-    [ObservableProperty] private Type? argType;
-    [ObservableProperty] private string? argValue;
-    [ObservableProperty] private string? argName;
+    [ObservableProperty] private Type argType = null!;
+    [ObservableProperty] private string argValue = string.Empty;
+    [ObservableProperty] private string argName = string.Empty;
+
+    public object TransformArg()
+    {
+        return argValue;
+    }
 }
