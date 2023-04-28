09e5c38ccc7d269e89b2395c846fb505b5ed0c00
Feast <feast107@gmail.com>
2023-04-28 10:24:43 +0800

    Create ConfigItemArgInfo.cs

---
+++ src/Mybatis-Plus-Generator.Definition/Abstractions/ConfigItemArgInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Mybatis_Plus_Generator.Definition.Abstractions;
+
+public partial class ConfigItemArgInfo : ObservableObject
+{
+    [ObservableProperty] private Type? argType;
+    [ObservableProperty] private string? argValue;
+    [ObservableProperty] private string? argName;
+}
