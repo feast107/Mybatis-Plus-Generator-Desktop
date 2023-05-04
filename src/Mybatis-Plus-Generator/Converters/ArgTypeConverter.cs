0710fa91cdbca1c79b3ce5d808cde02d094e700e
Feast <feast107@gmail.com>
2023-05-04 13:20:14 +0800

    Update ArgTypeConverter.cs

--- src/Mybatis-Plus-Generator/Converters/ArgTypeConverter.cs
+++ src/Mybatis-Plus-Generator/Converters/ArgTypeConverter.cs
@@ -1,50 +1,25 @@
-﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
+﻿using Mybatis_Plus_Generator.Definition.Enums;
+using System;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
-using Mybatis_Plus_Generator.Definition.Abstractions;
 
 namespace Mybatis_Plus_Generator.Converters;
 
-internal abstract class ArgTypeConverter : IValueConverter
+internal class ArgTypeConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return value is ConfigItemArgInfo info && Condition(info) ? Visibility.Visible : Visibility.Collapsed;
+        return value is ArgModes info
+               && parameter is ArgModes type
+               && info == type
+            ? Visibility.Visible
+            : Visibility.Collapsed;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
         throw new NotSupportedException();
     }
-
-    protected abstract bool Condition(ConfigItemArgInfo value);
 }
 
-internal class TypeText2VisibilityConverter : ArgTypeConverter
-{
-    protected override bool Condition(ConfigItemArgInfo value)
-    {
-        return value.ArgType == typeof(string) && !value.IsPassword;
-    }
-}
-internal class TypePassword2VisibilityConverter : ArgTypeConverter
-{
-    protected override bool Condition(ConfigItemArgInfo value)
-    {
-        return value.ArgType == typeof(string) && value.IsPassword;
-    }
-}
-
-internal class TypeSelect2VisibilityConverter : ArgTypeConverter
-{
-    protected override bool Condition(ConfigItemArgInfo value)
-    {
-        return value.ArgType.IsInterface;
-    }
-}
